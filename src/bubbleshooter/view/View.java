6a981982682010d75fc79066a26a5791d55e36ed
accursigiacomo <giacomo.accursi@studio.unibo.it>
2020-03-19 22:13:44 +0100

    added View and ViewImpl

--- src/bubbleshooter/view/View.java
+++ src/bubbleshooter/view/View.java
@@ -1,5 +1,14 @@
 package bubbleshooter.view;
 
+import bubbleshooter.view.scene.FXMLPath;
+import bubbleshooter.controller.Controller;
+
 public interface View {
+
+    void launch(Controller controller);
+    
+    void loadScene(FXMLPath scene);
+    
+    void showGameOver();
 
 }
