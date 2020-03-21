482a3aae32a24e3f584aa0c74245ae6726f95fa9
andrea_acampora <andrea.acampora@studio.unibo.it>
2020-03-21 18:07:49 +0100

    Created Controller interface with some methods

--- src/bubbleshooter/controller/Controller.java
+++ src/bubbleshooter/controller/Controller.java
@@ -2,4 +2,17 @@ package bubbleshooter.controller;
 
 public interface Controller {
 
+    void startGame();
+    
+    void pause();
+    
+    void resume();
+    
+    void setGameOver();
+    
+    //DATA MANAGER FOR SCORE
+    
+    //INPUT MANAGER
+    
+    //MUSIC 
 }
