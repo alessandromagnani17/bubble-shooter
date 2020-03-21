5a397e9d9a9da4c9348700b09885c45382d5282d
andrea_acampora <andrea.acampora@studio.unibo.it>
2020-03-21 18:13:31 +0100

    Created ModelImpl class

--- src/bubbleshooter/model/ModelImpl.java
+++ src/bubbleshooter/model/ModelImpl.java
@@ -1,5 +1,34 @@
 package bubbleshooter.model;
 
+import java.util.List;
+
+import bubbleshooter.model.gamemodality.GameStatus;
+import bubbleshooter.model.gameobject.GameObject;
+
 public class ModelImpl implements Model {
+
+    @Override
+    public void startGame() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<GameObject> getGameObjects() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void update(double elapsed) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public GameStatus getGameStatus() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
