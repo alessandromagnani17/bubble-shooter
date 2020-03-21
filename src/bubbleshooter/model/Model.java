0985d5bf9cdd299c5f26569fa3ec780e365441d2
andrea_acampora <andrea.acampora@studio.unibo.it>
2020-03-21 18:13:15 +0100

    Created Model interface with some methods

--- src/bubbleshooter/model/Model.java
+++ src/bubbleshooter/model/Model.java
@@ -1,5 +1,20 @@
 package bubbleshooter.model;
 
+import java.util.List;
+
+import bubbleshooter.model.gamemodality.GameStatus;
+import bubbleshooter.model.gameobject.GameObject;
+
+
 public interface Model {
 
+    void startGame();
+    
+    List<GameObject> getGameObjects();
+
+    void update(double elapsed);
+
+    GameStatus getGameStatus();
+
+    
 }
