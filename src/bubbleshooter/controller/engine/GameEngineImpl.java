750a569f813a0031ca526222fab677a1acbdcc63
andrea_acampora <andrea.acampora@studio.unibo.it>
2020-03-22 22:58:43 +0100

    Created GameEngineImpl class

---
+++ src/bubbleshooter/controller/engine/GameEngineImpl.java
@@ -0,0 +1,10 @@
+package bubbleshooter.controller.engine;
+
+public class GameEngineImpl  {
+
+    public static final int FPS = 60;
+    private static final int SECOND = 1000;
+    private static final int PERIOD = SECOND / FPS;
+    
+    
+}
