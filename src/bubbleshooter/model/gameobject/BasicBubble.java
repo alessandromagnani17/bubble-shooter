0f6f77a9c18458e8b58f66953c61042d826aaf10
accursigiacomo <giacomo.accursi@studio.unibo.it>
2020-03-25 00:36:11 +0100

    added method update to BasicBubble

--- src/bubbleshooter/model/gameobject/BasicBubble.java
+++ src/bubbleshooter/model/gameobject/BasicBubble.java
@@ -1,82 +1,20 @@
 package bubbleshooter.model.gameobject;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.util.GeometricShapeFactory;
 
-import bubbleshooter.view.images.Color;
-import javafx.geometry.Point2D;
+public class BasicBubble extends AbstractGameObject  {
 
-public class BasicBubble extends AbstractGameObject implements Bubble {
+    
+    
+    private Geometry shape; 
 
-    private final double radius = 10;
-    private Map<Direction, Optional<Bubble>> connections = new HashMap<>();
-    private Color color;
-    private Geometry shape;
+    
 
-    public BasicBubble(final Coordinate position, final Color color) {
-        super.setPosition(position);
-        this.color = color;
-        super.setHeigth(20); //DA MODIFICARE
-        super.setWidth(20); //DA MODIFICARE
-        this.shape = this.setCollisionBox();
-    }
-
-    @Override
-    public void connectBoth(final Direction direction, final Optional<Bubble> bubbleToConnect) {
-        this.setBubbleAt(direction, bubbleToConnect);
-        if (bubbleToConnect.isPresent()) {
-            bubbleToConnect.get().setBubbleAt(direction.opposite(), Optional.of(this));
-        }
-    }
-
-    @Override
-    public  Optional<Bubble> getBubbleAt(final Direction direction) {
-        return this.connections.get(direction).isPresent() ? this.connections.get(direction) : Optional.empty(); 
-    }
-
-    @Override
-    public void setBubbleAt(final Direction direction, final Optional<Bubble> bubble) {
-        if (!bubble.isPresent()) {
-            this.connections.put(direction, Optional.empty());
-        } else {
-            this.connections.put(direction, bubble);
-        }
-    }
-
-    @Override
-    public boolean hasBubbleAt(final Direction direction) {
-        return this.connections.get(direction).isPresent();
-    }
-
-    @Override
-    public void setColor(final Color bubbleColor) {
-        this.color = bubbleColor;
-    }
-
-    @Override
-    public Color getColor() {
-        return this.color;
-    }
-
-    @Override
-    public Collection<Bubble> getNeighbours() {
-        return this.connections.values()
-                .stream()
-                .filter(a -> a.isPresent())
-                .map(a -> a.get())
-                .collect(Collectors.toSet());
-    }
-
-    @Override
-    public Map<Direction, Optional<Bubble>> getConnections() {
-        return this.connections;
+    public BasicBubble(Coordinate position, double width, double heigth){
+        super(GameObjectsTypes.BASICBUBBLE, position, width, heigth);
+        this.shape = this.setCollisionBox(); 
     }
 
     @Override
@@ -89,8 +27,11 @@ public class BasicBubble extends AbstractGameObject implements Bubble {
 
     @Override
     public Geometry getCollisionBox() {
-        return this.shape;
+        return this.shape; 
     }
 
-
+    @Override
+    public void update(double elapsed) {
+        //todo
+    }
 }
