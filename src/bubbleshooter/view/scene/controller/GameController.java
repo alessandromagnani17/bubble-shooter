b7885d1152c36fd3897286e421bc7535d03d438f
giacomo accursi <giacomo.accursi@gmail.com>
2020-03-28 15:38:46 +0100

    added init method to the GameController

--- src/bubbleshooter/view/scene/controller/GameController.java
+++ src/bubbleshooter/view/scene/controller/GameController.java
@@ -1,8 +1,27 @@
 package bubbleshooter.view.scene.controller;
 
+import bubbleshooter.controller.Controller;
+import bubbleshooter.view.View;
+import bubbleshooter.view.rendering.CanvasDrawer;
 import bubbleshooter.view.scene.FXMLPath;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 
 public class GameController extends AbstractController{
+    
+    @FXML
+    private Canvas canvas;
+    
+    private CanvasDrawer canvasDrawer; 
+    private boolean isGameOver; 
+    
+    
+    @Override
+    public void init(final Controller controller, final View view) {
+        super.init(controller, view);
+        this.canvasDrawer = new CanvasDrawer(canvas); 
+        canvasDrawer.draw(this.getController().getGameObjects()); 
+    }
 
     @Override
     public FXMLPath getNextScene() {
