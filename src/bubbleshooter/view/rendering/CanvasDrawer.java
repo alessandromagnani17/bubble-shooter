aea8620f6c45d66ba728d467edb58c688a316c9a
giacomo accursi <giacomo.accursi@gmail.com>
2020-03-28 15:34:32 +0100

    added draw method to the CanvasDrawer

--- src/bubbleshooter/view/rendering/CanvasDrawer.java
+++ src/bubbleshooter/view/rendering/CanvasDrawer.java
@@ -1,6 +1,32 @@
 package bubbleshooter.view.rendering;
 
-public class CanvasDrawer {
+import java.util.List;
 
+
+import bubbleshooter.model.gameobject.GameObject;
+import javafx.scene.canvas.Canvas;
+
+public class CanvasDrawer {
+    
+    private final Canvas canvas;
+
+    public CanvasDrawer(Canvas canvas) {
+        this.canvas = canvas;
+    } 
+    
+    public void draw(final List<GameObject> gameObjects) {
+        gameObjects.forEach(o -> {
+            this.generateSprite(o).render();      
+        });
+    }
+    
+    private Sprite generateSprite(final GameObject gameObject) {
+        return new SpriteImpl(this.canvas.getGraphicsContext2D(), gameObject);
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+    
     
 }
