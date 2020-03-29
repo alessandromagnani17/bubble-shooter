9aeb26a23355e1337a0e3577985e412652447ea9
andrea_acampora <andrea.acampora@studio.unibo.it>
2020-03-29 23:31:32 +0200

    Created GameCostants class

---
+++ src/bubbleshooter/utility/GameCostants.java
@@ -0,0 +1,26 @@
+package bubbleshooter.utility;
+
+public enum GameCostants {
+
+    RADIUS(30),
+
+    GUIWIDTH(1000),
+
+    GUIHEIGTH(800),
+    
+    BUBBLESPEED(0.1),
+
+    FPS(60),
+
+    SECONDS(1000);
+
+    private double val;
+
+    GameCostants(double val) {
+        this.val = val;
+    }
+
+    public double getValue() {
+        return this.val;
+    }
+}
