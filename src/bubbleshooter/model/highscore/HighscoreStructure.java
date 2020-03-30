147904d753df9113f42c6c735b93934ded735f02
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-03-30 20:58:22 +0200

    Add HighscoreStructure

---
+++ src/bubbleshooter/model/highscore/HighscoreStructure.java
@@ -0,0 +1,27 @@
+package bubbleshooter.model.highscore;
+
+public class HighscoreStructure extends Score{
+
+    private static final long serialVersionUID = -4128959199732910429L;
+    private final String username;
+    
+    /**
+     * Construct a new HighscoreStructure
+     * @param name 
+     *              the player username
+     * @param score 
+     *              the score made by the player
+     */
+    public HighscoreStructure(final String name, final long score) {
+        super(score);
+        this.username = name;
+    }
+    
+    /**
+     * @return the current username of the player
+     */
+    public String getUsername() {
+        return this.username;
+    }
+
+}
