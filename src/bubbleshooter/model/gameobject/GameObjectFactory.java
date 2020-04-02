df2e400c78e507b43d36c07fe483dcbb388508d5
giacomo accursi <giacomo.accursi@gmail.com>
2020-04-03 00:47:45 +0200

    created GameObjectFactory

---
+++ src/bubbleshooter/model/gameobject/GameObjectFactory.java
@@ -0,0 +1,14 @@
+package bubbleshooter.model.gameobject;
+
+import javafx.geometry.Point2D;
+
+public class GameObjectFactory {
+    
+    public GameObject createBasicBubble(Point2D position) {
+        return new BasicBubble(position); 
+    }
+    public GameObject createShootingBubble(Point2D position) {
+        return new BasicBubble(position); 
+    }
+
+}
