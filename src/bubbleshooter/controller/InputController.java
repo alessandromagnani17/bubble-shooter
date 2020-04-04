4b7330c90b6166fc4c9061668f20da19b5af6bd7
Your Name <alessandro.magnani18@studio.unibo.it>
2020-04-04 19:39:57 +0200

    modified some parameters

--- src/bubbleshooter/controller/InputController.java
+++ src/bubbleshooter/controller/InputController.java
@@ -1,35 +1,13 @@
 package bubbleshooter.controller;
 
-import java.util.LinkedList;
 import javafx.scene.image.ImageView;
-import java.util.List;
-
-import javax.swing.JDialog;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Point2D;
-import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Shape;
 import javafx.stage.Stage; 
 
 public class InputController extends Application {
@@ -37,11 +15,11 @@ public class InputController extends Application {
     Pane root = new Pane();
     private ImageView cannon = new ImageView(new Image("bubbles/cannon.png"));
 
-    public InputController (Pane root) {
+    public InputController(Pane root) {
         this.root = root;
     }
 
-    public final void setCannonInitiallyPosition () {
+    public final void setCannonInitiallyPosition() {
         cannon.setScaleX(0.4);
         cannon.setScaleY(0.3);
         cannon.setLayoutX(350);
