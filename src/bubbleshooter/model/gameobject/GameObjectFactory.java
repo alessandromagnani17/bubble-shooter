e79e6eed79d48c57d253d71a5e434491d29515be
giacomo accursi <giacomo.accursi@gmail.com>
2020-04-10 22:25:22 +0200

    modified createBubble method

--- src/bubbleshooter/model/gameobject/GameObjectFactory.java
+++ src/bubbleshooter/model/gameobject/GameObjectFactory.java
@@ -1,15 +1,15 @@
 package bubbleshooter.model.gameobject;
-
 import javafx.geometry.Point2D;
 
 public class GameObjectFactory {
-
+    
+  
     public GameObject createBasicBubble(Point2D position) {
-        return new BasicBubble(position, Property.getRandomColor());
+        return new BasicBubble(position);
     }
 
     public GameObject createShootingBubble(Point2D position) {
-        return new ShootingBubble(position, Property.getRandomColor());
+        return new ShootingBubble(position);
     }
 
 }
