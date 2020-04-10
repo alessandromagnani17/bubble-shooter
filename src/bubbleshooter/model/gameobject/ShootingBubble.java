c6c00883d5a9f6e5c3e5476454daa8a7e8fd749c
giacomo accursi <giacomo.accursi@gmail.com>
2020-04-10 22:26:48 +0200

    deleted unused methods

--- src/bubbleshooter/model/gameobject/ShootingBubble.java
+++ src/bubbleshooter/model/gameobject/ShootingBubble.java
@@ -4,36 +4,9 @@ import javafx.geometry.Point2D;
 
 
 public class ShootingBubble extends BasicBubble  {
-
     
-    private GameObjectsTypes type; 
-    
-    public ShootingBubble(Point2D position, Property property) {
-        super(position, property);
+    public ShootingBubble(Point2D position) {
+        super(position);
         super.setType(GameObjectsTypes.MOVINGBUBBLE); 
     }
-    
-    @Override
-    public GameObjectsTypes getType() {
-        return this.type;
-    }
-
-    @Override
-    public void setType(final GameObjectsTypes type) {
-        this.type = type;
-    }
-
-    /*private Vector2D direction;
-    
-    public ShootingBubble(final Coordinate position, final Color color) {
-        super(position, color);
-    }
-
-    public final void setDirection(final Vector2D direction) {
-        this.direction = direction;
-    }
-    
-    public final Vector2D getDirection() {
-        return this.direction;
-    }*/
 }
