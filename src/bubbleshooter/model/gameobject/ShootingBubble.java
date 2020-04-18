61d30656b5e77a2f486b32ada2bcbac6c93f54e9
andrea_acampora <andrea.acampora@studio.unibo.it>
2020-04-19 00:08:57 +0200

    Changed method to calculate the direction of the shootingBubble

--- src/bubbleshooter/model/gameobject/ShootingBubble.java
+++ src/bubbleshooter/model/gameobject/ShootingBubble.java
@@ -6,23 +6,25 @@ import javafx.geometry.Point2D;
 public class ShootingBubble extends BasicBubble {
 
     private Point2D shootingDirection;
-
+    
     public ShootingBubble(final Point2D position) {
         super(position);
         super.setType(GameObjectsTypes.SHOOTINGBUBBLE);
-        this.shootingDirection = this.getPosition();
+        this.shootingDirection = super.getPosition();
     }
 
     public final void setDirection(final Point2D direction) {
         this.shootingDirection = direction;
     }
-
+    
     public final Point2D getDirection() {
         return this.shootingDirection;
     }
 
-    public final void update(final double elapsed) {
-    	super.setPosition(super.getPosition().add(this.shootingDirection));
+    public final void update(final double elapsed) { 
+      if (!this.shootingDirection.equals(this.getPosition())) {
+        super.setPosition(super.getPosition().add(this.shootingDirection.multiply(elapsed)));
+      }
     }
 
 }
