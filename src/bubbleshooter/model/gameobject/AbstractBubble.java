6b33804c6a7fb5b6d3ffe7d0d8a75808f038409a
giacomo accursi <giacomo.accursi@gmail.com>
2020-04-22 23:59:56 +0200

    modified constructor end getter methods

--- src/bubbleshooter/model/gameobject/AbstractBubble.java
+++ src/bubbleshooter/model/gameobject/AbstractBubble.java
@@ -1,16 +1,72 @@
 package bubbleshooter.model.gameobject;
+
+import bubbleshooter.utility.GameCostants;
 import javafx.geometry.Point2D;
 
-public class BasicBubble extends AbstractGameObject {
-    
-    public BasicBubble(Point2D position) {
-        super(position); 
-        super.setType(GameObjectsTypes.BASICBUBBLE);
-    }
+public abstract class AbstractBubble implements Bubble {
 
-    @Override
-    public GameObjectsTypes getType() {
-        return super.getType();
-    }
+	private Point2D position;
+	private boolean isDestroyed;
+	private double radius;
+	private BubbleType type;
+	private BubbleColor color;
+
+	public AbstractBubble(BubbleType type, Point2D position) {
+		this.isDestroyed = false;
+		this.radius = GameCostants.RADIUS.getValue();
+
+	}
+
+	@Override
+	public Point2D getPosition() {
+		return this.position;
+	}
+
+	@Override
+	public void setPosition(Point2D position) {
+		this.position = position;
+
+	}
+
+	@Override
+	public boolean isDestroyed() {
+		return this.isDestroyed;
+	}
+
+	@Override
+	public void destroy() {
+		this.isDestroyed = true;
+
+	}
+
+	@Override
+	public double getRadius() {
+		return this.radius;
+	}
+
+	@Override
+	public void setRadius(double radius) {
+		this.radius = radius;
+
+	}
+
+	@Override
+	public BubbleType getType() {
+		return this.type;
+	}
+
+	public void setType(BubbleType type) {
+		this.type = type;
+	}
+
+	@Override
+	public BubbleColor getColor() {
+		return this.color;
+	}
+
+	@Override
+	public void setColor(BubbleColor color) {
+		this.color = color;
+	}
 
 }
