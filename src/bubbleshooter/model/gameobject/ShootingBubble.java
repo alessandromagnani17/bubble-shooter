2999a695e94631aee4700340759f154c662af428
giacomo accursi <giacomo.accursi@gmail.com>
2020-04-22 23:57:37 +0200

    modify constructor

--- src/bubbleshooter/model/gameobject/ShootingBubble.java
+++ src/bubbleshooter/model/gameobject/ShootingBubble.java
@@ -2,29 +2,26 @@ package bubbleshooter.model.gameobject;
 
 import javafx.geometry.Point2D;
 
+public class ShootingBubble extends AbstractBubble {
 
-public class ShootingBubble extends BasicBubble {
+	private Point2D shootingDirection;
 
-    private Point2D shootingDirection;
-    
-    public ShootingBubble(final Point2D position) {
-        super(position);
-        super.setType(GameObjectsTypes.SHOOTINGBUBBLE);
-        this.shootingDirection = super.getPosition();
-    }
+	public ShootingBubble(Point2D position) {
+		super(BubbleType.SHOOTING_BUBBLE, position);
+	}
 
-    public final void setDirection(final Point2D direction) {
-        this.shootingDirection = direction;
-    }
-    
-    public final Point2D getDirection() {
-        return this.shootingDirection;
-    }
+	public final void setDirection(final Point2D direction) {
+		this.shootingDirection = direction;
+	}
 
-    public final void update(final double elapsed) { 
-      if (!this.shootingDirection.equals(this.getPosition())) {
-        super.setPosition(super.getPosition().add(this.shootingDirection.multiply(elapsed)));
-      }
-    }
+	public final Point2D getDirection() {
+		return this.shootingDirection;
+	}
+
+	public final void update(final double elapsed) {
+		if (!this.shootingDirection.equals(this.getPosition())) {
+			super.setPosition(super.getPosition().add(this.shootingDirection.multiply(elapsed)));
+		}
+	}
 
 }
