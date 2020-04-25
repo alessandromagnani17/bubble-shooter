f59ddd8ef666e73c809d570353130d199947dd79
Your Name <alessandro.magnani18@studio.unibo.it>
2020-04-25 11:04:28 +0200

    Created class GameOver

---
+++ src/bubbleshooter/controller/GameOver.java
@@ -0,0 +1,14 @@
+package bubbleshooter.controller;
+
+public class GameOver {
+
+    private boolean isGameOver = false;
+
+    public final boolean isGameOver() {
+        return isGameOver;
+    }
+
+    public final void setGameOver(final boolean isGameOver) {
+        this.isGameOver = isGameOver;
+    }
+}
