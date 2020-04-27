b183fe0ffbc3e43f7869982718ea5d75f75eabaa
Your Name <alessandro.magnani18@studio.unibo.it>
2020-04-27 21:29:16 +0200

    Added objects Cannon and Rotate into GameController

--- src/bubbleshooter/view/scene/controller/GameController.java
+++ src/bubbleshooter/view/scene/controller/GameController.java
@@ -1,11 +1,13 @@
 package bubbleshooter.view.scene.controller;
 
 import bubbleshooter.controller.Controller;
+import bubbleshooter.controller.HandlerAdapterMouseClicked;
+import bubbleshooter.controller.HandlerAdapterMouseMoved;
 import bubbleshooter.model.gameobject.Bubble;
 import bubbleshooter.model.gameobject.BubbleType;
-import bubbleshooter.model.gameobject.ShootingBubble;
 import bubbleshooter.utility.PhysicHelper;
 import bubbleshooter.view.View;
+import bubbleshooter.view.images.ImagePath;
 import bubbleshooter.view.rendering.CanvasDrawer;
 import bubbleshooter.view.scene.FXMLPath;
 import javafx.event.EventHandler;
@@ -13,39 +15,61 @@ import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.CheckBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.transform.Rotate;
 
 public class GameController extends AbstractController {
 
-	@FXML
-	private Canvas canvas;
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private AnchorPane pane;
+
     @FXML
     private CheckBox help = new CheckBox("Help");
-	private CanvasDrawer canvasDrawer;
-	private boolean gameOver;
+    private CanvasDrawer canvasDrawer;
+    private boolean gameOver;
 
     @Override
-    public void init(final Controller controller, final View view) {
+    public final void init(final Controller controller, final View view) {
         super.init(controller, view);
+
+        ImageView cannon = new ImageView(new Image(ImagePath.CANNON.getPath()));
+
+        cannon.setLayoutX(315.5);
+        cannon.setLayoutY(460.0);
+
+
+        Rotate rotation = new Rotate();
+        rotation.setPivotX(this.getController().getBubbles().stream().filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE)).findFirst().get().getPosition().getX() - cannon.getLayoutX());
+        rotation.setPivotY(this.getController().getBubbles().stream().filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE)).findFirst().get().getPosition().getY() - cannon.getLayoutY());
+        cannon.getTransforms().add(rotation);
+
+        
+
         this.canvasDrawer = new CanvasDrawer(this.canvas);
         //canvasDrawer.draw(this.getController().getBubbles());
         getController().resume();
-		this.canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(final MouseEvent event) {
-				Bubble shootingBubble = getController().getBubbles().stream().filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE)).findFirst().get();
-				shootingBubble.setDirection(PhysicHelper.calculateShootingDirection(
-						new Point2D(event.getX(), event.getY()), shootingBubble.getPosition()));
-			}
-		});
-	}
+        this.canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(final MouseEvent event) {
+                Bubble shootingBubble = getController().getBubbles().stream().filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE)).findFirst().get();
+                shootingBubble.setDirection(PhysicHelper.calculateShootingDirection(
+                         new Point2D(event.getX(), event.getY()), shootingBubble.getPosition()));
+            }
+        });
+    }
 
 
-	public void render() {
-		if (this.isGameOver()) {
-			this.nextScene();
-		}
-		if(this.help.isSelected()) {
+    public final void render() {
+        if (this.isGameOver()) {
+            this.nextScene();
+        }
+		if (this.help.isSelected()) {
 			//Disegnare la linea tratteggiata
 		}
 		// da aggiungere anche la chiamata al controller per sapere lo score corrente
