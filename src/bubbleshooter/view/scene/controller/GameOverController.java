09d6bfa3e4d6c6cae519cea7028702e5b8a87367
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-04-28 17:44:22 +0200

    Add some body to method addToHighscore()

--- src/bubbleshooter/view/scene/controller/GameOverController.java
+++ src/bubbleshooter/view/scene/controller/GameOverController.java
@@ -1,16 +1,16 @@
 package bubbleshooter.view.scene.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
+import bubbleshooter.model.highscore.HighscoreStoreImpl;
 import bubbleshooter.view.scene.FXMLPath;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
-public class GameOverController extends AbstractController implements Initializable {
+public class GameOverController extends AbstractController {
 	
-	@FXML private TextArea textArea;
+	@FXML private TextArea textArea = new TextArea();
+	@FXML private Button addButton;
+	private HighscoreStoreImpl highscoreStore;
 
 	@Override
 	public FXMLPath getNextScene() {
@@ -24,13 +24,13 @@ public class GameOverController extends AbstractController implements Initializa
 	
 	public void addToHighscore() {
 		if(!this.textArea.getText().isEmpty()) {
+			this.highscoreStore = new HighscoreStoreImpl();
 			// Da creare e aggiungere l'highscore
+			// HighscoreStructure store = new HighscoreStructure(this.textArea.getText(), , LevelTypes.SURVIVALMODE)
+			// this.highscoreStore.addScore(LevelTypes.SURVIVALMODE, store);
+			
+			this.getView().loadScene(FXMLPath.HIGHSCORE);
 		}
-	}
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		
 	}
 
 }
