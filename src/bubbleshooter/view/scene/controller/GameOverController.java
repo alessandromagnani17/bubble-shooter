53ab84471dcafefd1c93c0d9e2f0b6fee285d5d9
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-04-28 17:09:14 +0200

    Create method addToHighscore()

--- src/bubbleshooter/view/scene/controller/GameOverController.java
+++ src/bubbleshooter/view/scene/controller/GameOverController.java
@@ -1,8 +1,16 @@
 package bubbleshooter.view.scene.controller;
 
-import bubbleshooter.view.scene.FXMLPath;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class GameOverController extends AbstractController {
+import bubbleshooter.view.scene.FXMLPath;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
+public class GameOverController extends AbstractController implements Initializable {
+	
+	@FXML private TextArea textArea;
 
 	@Override
 	public FXMLPath getNextScene() {
@@ -12,6 +20,17 @@ public class GameOverController extends AbstractController {
 	@Override
 	protected FXMLPath getPreviousScene() {
 		return FXMLPath.GAME;
+	}
+	
+	public void addToHighscore() {
+		if(!this.textArea.getText().isEmpty()) {
+			// Da creare e aggiungere l'highscore
+		}
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
 	}
 
 }
