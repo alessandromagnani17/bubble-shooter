2121ca99592fd03a41a02b4a453b82efd7277914
giacomo accursi <giacomo.accursi@gmail.com>
2020-04-29 14:35:05 +0200

    remove resume method from init

--- src/bubbleshooter/view/scene/controller/GameController.java
+++ src/bubbleshooter/view/scene/controller/GameController.java
@@ -14,6 +14,9 @@ import bubbleshooter.view.View;
 import bubbleshooter.view.images.ImagePath;
 import bubbleshooter.view.rendering.CanvasDrawer;
 import bubbleshooter.view.scene.FXMLPath;
+import bubbleshooter.view.states.GameState;
+import bubbleshooter.view.states.InGameState;
+import bubbleshooter.view.states.InPauseState;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
@@ -30,69 +33,76 @@ public class GameController extends AbstractController {
 
 	private static final double LIMITS = 400.0;
 	private static final double LIMITS_MOUSE = 530.0;
-	
+
 	@FXML
 	private Canvas canvas;
-	
+
 	@FXML
-	private AnchorPane pane; 
-	
+	private AnchorPane pane;
+
 	@FXML
 	private CheckBox help = new CheckBox("Help");
 	private CanvasDrawer canvasDrawer;
 	private boolean gameOver;
+	private GameState currentState;
+	private GameState inGameState;
+	private GameState inPauseState;
 
+	@Override
+	public final void init(final Controller controller, final View view) {
+		super.init(controller, view);
 
-    @Override
-    public final void init(final Controller controller, final View view) {
-        super.init(controller, view);
+		this.canvasDrawer = new CanvasDrawer(this.canvas);
+		this.inGameState = new InGameState(this, controller);
+		this.inPauseState = new InPauseState(this, controller);
+		this.setCurrentState(this.inGameState);
 
-        ImageView cannon = new ImageView(new Image(ImagePath.CANNON.getPath()));
-        Rotate rotation = new Rotate();
-        double xBubble = getController().getBubbles().stream().filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE)).findFirst().get().getPosition().getX();
-        double yBubble = getController().getBubbles().stream().filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE)).findFirst().get().getPosition().getY();
+		// getController().resume();
 
-        cannon.setLayoutX(315.5);
-        cannon.setLayoutY(455.0);
+		ImageView cannon = new ImageView(new Image(ImagePath.CANNON.getPath()));
+		Rotate rotation = new Rotate();
+		double xBubble = getController().getBubbles().stream()
+				.filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE)).findFirst().get().getPosition().getX();
+		double yBubble = getController().getBubbles().stream()
+				.filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE)).findFirst().get().getPosition().getY();
 
-        rotation.setPivotX(xBubble - cannon.getLayoutX());
-        rotation.setPivotY(yBubble - cannon.getLayoutY());
-        cannon.getTransforms().add(rotation);
+		cannon.setLayoutX(315.5);
+		cannon.setLayoutY(455.0);
 
-        pane.getChildren().add(cannon);
+		rotation.setPivotX(xBubble - cannon.getLayoutX());
+		rotation.setPivotY(yBubble - cannon.getLayoutY());
+		cannon.getTransforms().add(rotation);
 
-        canvas.setOnMouseMoved(new HandlerAdapterMouseMoved(rotation, xBubble, yBubble));
-        canvas.setOnMouseDragged(new HandlerAdapterMouseMoved(rotation, xBubble, yBubble));
-        canvas.setOnMouseClicked(new HandlerAdapterMouseClicked(rotation, xBubble, yBubble));
+		pane.getChildren().add(cannon);
 
-        this.canvasDrawer = new CanvasDrawer(this.canvas);
-        //canvasDrawer.draw(this.getController().getBubbles());
-        getController().resume();
-        this.canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+		canvas.setOnMouseMoved(new HandlerAdapterMouseMoved(rotation, xBubble, yBubble));
+		canvas.setOnMouseDragged(new HandlerAdapterMouseMoved(rotation, xBubble, yBubble));
+		canvas.setOnMouseClicked(new HandlerAdapterMouseClicked(rotation, xBubble, yBubble));
+		this.canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 
-        	@Override
-            public void handle(final MouseEvent event) {
-                GameOverController gameOverController = new GameOverController(getController().getBubbles(), LIMITS);
-                Bubble shootingBubble = getController().getBubbles().stream().filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE)).findFirst().get();
-                if (shootingBubble.getPosition().getX() == xBubble && event.getY() < LIMITS_MOUSE) {
-                	shootingBubble.setDirection(PhysicHelper.calculateShootingDirection(
-                            new Point2D(event.getX(), event.getY()), shootingBubble.getPosition()));
-                }
+			@Override
+			public void handle(final MouseEvent event) {
+				GameOverController gameOverController = new GameOverController(getController().getBubbles(), LIMITS);
+				Bubble shootingBubble = getController().getBubbles().stream()
+						.filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE)).findFirst().get();
+				if (shootingBubble.getPosition().getX() == xBubble && event.getY() < LIMITS_MOUSE) {
+					shootingBubble.setDirection(PhysicHelper.calculateShootingDirection(
+							new Point2D(event.getX(), event.getY()), shootingBubble.getPosition()));
+				}
 
-        		if (gameOverController.isGameOver()) {
-        			setGameOver();
-        		}
-            }
-        });
-    }
+				if (gameOverController.isGameOver()) {
+					setGameOver();
+				}
+			}
+		});
+	}
 
-
-    public final void render() {
-        if (this.isGameOver()) {
-            this.nextScene();
-        }
+	public final void render() {
+		if (this.isGameOver()) {
+			this.nextScene();
+		}
 		if (this.help.isSelected()) {
-			//Disegnare la linea tratteggiata
+			// Disegnare la linea tratteggiata
 		}
 		// da aggiungere anche la chiamata al controller per sapere lo score corrente
 		this.clearCanvas();
@@ -116,12 +126,31 @@ public class GameController extends AbstractController {
 	public final void setGameOver() {
 		this.gameOver = true;
 	}
-		
-    // Clear the canvas after every render. It avoids ghosting effect.
-    private void clearCanvas() {
-        this.canvas.getGraphicsContext2D().restore();
-    	this.canvas.getGraphicsContext2D().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-    }
-    
+
+	// Clear the canvas after every render. It avoids ghosting effect.
+	private void clearCanvas() {
+		this.canvas.getGraphicsContext2D().restore();
+		this.canvas.getGraphicsContext2D().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+	}
+
+	public GameState getCurrentState() {
+		return currentState;
+	}
+
+	public void setCurrentState(GameState state) {
+		if (currentState != null) {
+			this.currentState.exit();
+		}
+		this.currentState = state;
+		this.currentState.enter();
+	}
+
+	public GameState getInGameState() {
+		return inGameState;
+	}
+
+	public GameState getInPauseState() {
+		return inPauseState;
+	}
 
 }
