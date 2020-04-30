1d11377e3f816e536895767af2abf8f6a55dc9f7
Andrea Acampora <andrea.acampora@studio.unibo.it>
2020-05-01 01:43:12 +0200

    Created TestBubbleCollision class

--- src/test/java/collision/TestBubbleCollision.java
+++ src/test/java/collision/TestBubbleCollision.java
@@ -1,14 +1,30 @@
 package collision;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+
+import bubbleshooter.model.collision.CollisionController;
+import bubbleshooter.model.gamemodality.BasicMode;
+import bubbleshooter.model.gameobject.Bubble;
+import bubbleshooter.model.gameobject.GridBubble;
+import bubbleshooter.model.gameobject.ShootingBubble;
+import javafx.geometry.Point2D;
+import javafx.scene.shape.Circle;
+
 public class TestBubbleCollision {
 
+	private BasicMode level = new BasicMode();
+    private CollisionController collisionController = new CollisionController(level);
 
+	
 	public void testCollision() {
 	}
+	
+
 	
 }
