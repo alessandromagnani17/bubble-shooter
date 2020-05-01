0f57f37bd900401229a2e0c30bf1ca21b16b025a
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-01 21:08:06 +0200

    Add method init for print the score

--- src/main/java/bubbleshooter/view/scene/controller/GameOverController.java
+++ src/main/java/bubbleshooter/view/scene/controller/GameOverController.java
@@ -1,5 +1,7 @@
 package bubbleshooter.view.scene.controller;
 
+import bubbleshooter.controller.Controller;
+import bubbleshooter.view.View;
 import bubbleshooter.view.scene.FXMLPath;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -9,6 +11,12 @@ public class GameOverController extends AbstractController {
 	
 	@FXML private TextArea textArea = new TextArea();
 	@FXML private Label scoreLabel = new Label();
+	
+	@Override
+	public final void init(final Controller controller, final View view) {
+		super.init(controller, view);
+		this.scoreLabel.setText(String.valueOf(this.getController().getScore()));
+	}
 	
 	@Override
 	public FXMLPath getNextScene() {
