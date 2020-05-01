f5653caef0982ec8a9bd42454414e67748eec26b
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-01 16:52:59 +0200

    Add deleteLine method

--- src/main/java/bubbleshooter/view/scene/controller/DrawHelpLine.java
+++ src/main/java/bubbleshooter/view/scene/controller/DrawHelpLine.java
@@ -1,13 +1,29 @@
 package bubbleshooter.view.scene.controller;
 
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Line;
 
 public class DrawHelpLine {
 	
-	private AnchorPane pane;
+	private AnchorPane pane = new AnchorPane();
+	private Line helpLine = new Line(352.5, 574.0, 352.5, 0); 
+	private Line borderLeft = new Line(0, 0, 0, 700);
+	private Line borderRight = new Line(705, 0, 705, 700);
 
 	public DrawHelpLine(AnchorPane pane) {
 		this.pane = pane;
+	}
+
+	public void drawLine() {
+		//this.borderLeft.setVisible(false);
+		//this.borderRight.setVisible(false);
+		this.pane.getChildren().add(helpLine);
+		this.pane.getChildren().add(borderRight);
+		this.pane.getChildren().add(borderLeft);
+	}
+
+	public void deleteLine() {
+		this.helpLine.setVisible(false);
 	}
 
 }
