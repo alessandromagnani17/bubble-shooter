c617c6b004205e61437f348a75f99d0b42289288
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-01 17:05:51 +0200

    Modify constructor and add rotation

--- src/main/java/bubbleshooter/view/scene/controller/DrawHelpLine.java
+++ src/main/java/bubbleshooter/view/scene/controller/DrawHelpLine.java
@@ -2,6 +2,7 @@ package bubbleshooter.view.scene.controller;
 
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Line;
+import javafx.scene.transform.Rotate;
 
 public class DrawHelpLine {
 	
@@ -9,21 +10,28 @@ public class DrawHelpLine {
 	private Line helpLine = new Line(352.5, 574.0, 352.5, 0); 
 	private Line borderLeft = new Line(0, 0, 0, 700);
 	private Line borderRight = new Line(705, 0, 705, 700);
+	Rotate rotation = new Rotate();
 
 	public DrawHelpLine(AnchorPane pane) {
 		this.pane = pane;
+		this.borderLeft.setVisible(false);
+		this.borderRight.setVisible(false);
+		this.helpLine.setVisible(false);
+		this.pane.getChildren().add(borderRight);
+		this.pane.getChildren().add(borderLeft);
+		this.pane.getChildren().add(helpLine);
 	}
 
 	public void drawLine() {
-		//this.borderLeft.setVisible(false);
-		//this.borderRight.setVisible(false);
-		this.pane.getChildren().add(helpLine);
-		this.pane.getChildren().add(borderRight);
-		this.pane.getChildren().add(borderLeft);
+		this.helpLine.setVisible(true);
 	}
 
 	public void deleteLine() {
 		this.helpLine.setVisible(false);
+	}
+	
+	public Line getHelpLine() {
+		return this.helpLine;
 	}
 
 }
