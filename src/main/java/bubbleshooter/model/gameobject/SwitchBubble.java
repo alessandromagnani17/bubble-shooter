a390c312ac59cc55f761c253c2bcd6f995a2669c
Your Name <alessandro.magnani18@studio.unibo.it>
2020-05-01 12:46:03 +0200

    Created class SwitchBubble

---
+++ src/main/java/bubbleshooter/model/gameobject/SwitchBubble.java
@@ -0,0 +1,23 @@
+package bubbleshooter.model.gameobject;
+
+import bubbleshooter.model.component.ComponentType;
+import bubbleshooter.model.component.SwitchComponent;
+import javafx.geometry.Point2D;
+
+public class SwitchBubble extends AbstractBubble {
+
+	public SwitchBubble(BubbleType type, Point2D position) {
+		super(BubbleType.SWITCH_BUBBLE, position);
+	}
+
+	@Override
+	protected final void setComponents() {
+		this.addComponent(new SwitchComponent(this));
+	}
+	
+	@Override
+	public final void update(final double elapsed) {
+        this.getComponent(ComponentType.SWITCHCOMPONENT).get().update(elapsed);
+	}
+
+}
