b47c4bc73651b6e5a4f021fbd4143b58cd6c9594
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-01 17:08:39 +0200

    Add method setRtotation

--- src/main/java/bubbleshooter/view/scene/controller/DrawHelpLine.java
+++ src/main/java/bubbleshooter/view/scene/controller/DrawHelpLine.java
@@ -10,16 +10,23 @@ public class DrawHelpLine {
 	private Line helpLine = new Line(352.5, 574.0, 352.5, 0); 
 	private Line borderLeft = new Line(0, 0, 0, 700);
 	private Line borderRight = new Line(705, 0, 705, 700);
+	private static final double X_BUBBLE = 352.5;
+	private static final double Y_BUBBLE = 600.0;
 	Rotate rotation = new Rotate();
 
 	public DrawHelpLine(AnchorPane pane) {
 		this.pane = pane;
+		this.setRotation();
 		this.borderLeft.setVisible(false);
 		this.borderRight.setVisible(false);
 		this.helpLine.setVisible(false);
 		this.pane.getChildren().add(borderRight);
 		this.pane.getChildren().add(borderLeft);
 		this.pane.getChildren().add(helpLine);
+	}
+
+	private void setRotation() {
+		
 	}
 
 	public void drawLine() {
