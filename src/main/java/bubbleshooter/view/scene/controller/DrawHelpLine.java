81419b73d3fbac170d5aba1a41af761b8833f866
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-01 16:35:06 +0200

    Add class DrawHelpLine

---
+++ src/main/java/bubbleshooter/view/scene/controller/DrawHelpLine.java
@@ -0,0 +1,13 @@
+package bubbleshooter.view.scene.controller;
+
+import javafx.scene.layout.AnchorPane;
+
+public class DrawHelpLine {
+	
+	private AnchorPane pane;
+
+	public DrawHelpLine(AnchorPane pane) {
+		this.pane = pane;
+	}
+
+}
