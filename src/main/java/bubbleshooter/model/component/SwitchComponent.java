0cb6e64338f04e7d13c21ebea1434c74f14299e2
Your Name <alessandro.magnani18@studio.unibo.it>
2020-05-01 12:45:37 +0200

    Created class SwitchComponent

---
+++ src/main/java/bubbleshooter/model/component/SwitchComponent.java
@@ -0,0 +1,28 @@
+package bubbleshooter.model.component;
+
+import bubbleshooter.model.gameobject.Bubble;
+import bubbleshooter.model.gameobject.BubbleColor;
+
+public class SwitchComponent extends AbstractComponent {
+
+    private BubbleColor bubbleColor;
+	
+    public SwitchComponent(final Bubble container) {
+		super(container);
+		this.setBubbleColor(container.getColor());
+        this.setType(ComponentType.SWITCHCOMPONENT);
+	}
+
+    public void switchBubbles () {
+    	
+    }
+
+	public final BubbleColor getBubbleColor() {
+		return bubbleColor;
+	}
+
+	public final void setBubbleColor(final BubbleColor bubbleColor) {
+		this.bubbleColor = bubbleColor;
+	}
+
+}
