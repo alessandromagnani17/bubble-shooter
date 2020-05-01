5a7bacd61d3afae95d58daccc2e4c3598aae32cd
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-01 21:05:43 +0200

    Add @FXML variable for label

--- src/main/java/bubbleshooter/view/scene/controller/GameOverController.java
+++ src/main/java/bubbleshooter/view/scene/controller/GameOverController.java
@@ -2,11 +2,13 @@ package bubbleshooter.view.scene.controller;
 
 import bubbleshooter.view.scene.FXMLPath;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 public class GameOverController extends AbstractController {
 	
 	@FXML private TextArea textArea = new TextArea();
+	@FXML private Label scoreLabel = new Label();
 	
 	@Override
 	public FXMLPath getNextScene() {
