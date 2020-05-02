feba06964f5dfde4791a7c2883c524a4eb7c79b1
giacomo accursi <giacomo.accursi@gmail.com>
2020-05-02 19:30:27 +0200

    create getRemainingColors method

--- src/main/java/bubbleshooter/model/gameobject/BubbleGridHelper.java
+++ src/main/java/bubbleshooter/model/gameobject/BubbleGridHelper.java
@@ -9,28 +9,28 @@ import javafx.geometry.Point2D;
 
 public class BubbleGridHelper {
 
-    private final GameObjectManager gameObjectManager;
-    private static final double DIAGONALDISTANCE = Bubble.getWidth() * 1.20;
+	private final GameObjectManager gameObjectManager;
+	private static final double DIAGONALDISTANCE = Bubble.getWidth() * 1.20;
 
 	public BubbleGridHelper(final GameObjectManager gameObjectManager) {
 		this.gameObjectManager = gameObjectManager;
 	}
-	
+
 	public final List<Bubble> getBubbleNeighbours(final Bubble bubble) {
 		return this.getBubbleGrid().stream().filter(a -> this.isNear(a, bubble)).collect(Collectors.toList());
 	}
-	
+
 	public final List<Bubble> getBubbleGrid() {
 		return this.gameObjectManager.getBubbleGrid();
 	}
-	
+
 	public final double getDistanceBetweenBubbles(final Bubble bubbleAt, final Bubble bubbleTo) {
 		Point2D bubbleAtPos = bubbleAt.getPosition();
 		Point2D bubbleToPos = bubbleTo.getPosition();
-		return     Math.sqrt(Math.pow(bubbleAtPos.getX() - bubbleToPos.getX(), 2)
-				+  Math.pow(bubbleAtPos.getY() - bubbleToPos.getY(), 2));
+		return Math.sqrt(Math.pow(bubbleAtPos.getX() - bubbleToPos.getX(), 2)
+				+ Math.pow(bubbleAtPos.getY() - bubbleToPos.getY(), 2));
 	}
-	
+
 	public final boolean isNear(final Bubble bubbleAt, final Bubble bubbleTo) {
 		return this.getDistanceBetweenBubbles(bubbleAt, bubbleTo) <= DIAGONALDISTANCE
 				&& this.getDistanceBetweenBubbles(bubbleAt, bubbleTo) > 0;
@@ -39,7 +39,7 @@ public class BubbleGridHelper {
 	public final boolean areEquals(final Bubble a, final Bubble b) {
 		return a.getColor().equals(b.getColor());
 	}
-	
+
 	private List<Bubble> getLinkedBubbles(final Bubble starting, final List<Bubble> linkedBubbles) {
 		this.getBubbleNeighbours(starting).stream()
 				.filter(a -> !linkedBubbles.contains(a) && !a.isDestroyed() && linkedBubbles.add(a))
@@ -58,15 +58,22 @@ public class BubbleGridHelper {
 		}
 		return this.getBubbleGrid().stream().filter(a -> !linkedBubbles.contains(a)).collect(Collectors.toSet());
 	}
-	
+
 	public final Set<Point2D> getNeighbourPosition(final Bubble bubble) {
-        Point2D bubblePos = bubble.getPosition();
-        return Set.of(new Point2D(bubblePos.getX() - Bubble.getWidth(), bubblePos.getY()),
-                      new Point2D(bubblePos.getX() + Bubble.getWidth(), bubblePos.getY()),
-                      new Point2D(bubblePos.getX() - Bubble.getWidth() / 2, bubblePos.getY() - Bubble.getWidth()),
-                      new Point2D(bubblePos.getX() + Bubble.getWidth() / 2, bubblePos.getY() - Bubble.getWidth()),
-                      new Point2D(bubblePos.getX() - Bubble.getWidth() / 2, bubblePos.getY() + Bubble.getWidth()),
-                      new Point2D(bubblePos.getX() + Bubble.getWidth() / 2, bubblePos.getY() + Bubble.getWidth()));
-    }
+		Point2D bubblePos = bubble.getPosition();
+		return Set.of(new Point2D(bubblePos.getX() - Bubble.getWidth(), bubblePos.getY()),
+				new Point2D(bubblePos.getX() + Bubble.getWidth(), bubblePos.getY()),
+				new Point2D(bubblePos.getX() - Bubble.getWidth() / 2, bubblePos.getY() - Bubble.getWidth()),
+				new Point2D(bubblePos.getX() + Bubble.getWidth() / 2, bubblePos.getY() - Bubble.getWidth()),
+				new Point2D(bubblePos.getX() - Bubble.getWidth() / 2, bubblePos.getY() + Bubble.getWidth()),
+				new Point2D(bubblePos.getX() + Bubble.getWidth() / 2, bubblePos.getY() + Bubble.getWidth()));
+	}
+
+	public List<BubbleColor> getRemainingColors() {
+		System.out.println(this.getBubbleGrid().stream().filter(b -> b.getType().equals(BubbleType.GRID_BUBBLE))
+				.map(b -> b.getColor()).distinct().collect(Collectors.toList()));
+		return this.getBubbleGrid().stream().filter(b -> b.getType().equals(BubbleType.GRID_BUBBLE))
+				.map(b -> b.getColor()).distinct().collect(Collectors.toList());
+	}
 
 }
