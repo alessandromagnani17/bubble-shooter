4e9ca6a1fea0a7207a3ed91bdddbfb29e25e64f4
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-02 21:37:39 +0200

    Add method init()

--- src/main/java/bubbleshooter/view/scene/controller/PauseController.java
+++ src/main/java/bubbleshooter/view/scene/controller/PauseController.java
@@ -1,8 +1,17 @@
 package bubbleshooter.view.scene.controller;
 
+import bubbleshooter.controller.Controller;
+import bubbleshooter.view.View;
 import bubbleshooter.view.scene.FXMLPath;
 
 public class PauseController extends AbstractController {
+	
+	@Override
+	public final void init(final Controller controller, final View view) {
+		super.init(controller, view);
+		
+		
+	}
 
 	@Override
 	public FXMLPath getNextScene() {
