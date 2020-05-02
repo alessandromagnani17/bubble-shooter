d6e9be0f140abe6f88cede4f4aff943bb7932034
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-02 15:08:35 +0200

    Add layout property for game over label

--- src/main/java/bubbleshooter/view/scene/controller/GameOverController.java
+++ src/main/java/bubbleshooter/view/scene/controller/GameOverController.java
@@ -1,12 +1,15 @@
 package bubbleshooter.view.scene.controller;
 
 import bubbleshooter.controller.Controller;
+import bubbleshooter.utility.Settings;
 import bubbleshooter.view.View;
 import bubbleshooter.view.scene.FXMLPath;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
 
 public class GameOverController extends AbstractController {
 	
@@ -20,12 +23,26 @@ public class GameOverController extends AbstractController {
 	@FXML private TextArea textArea = new TextArea();
 	@FXML private Button buttonAdd = new Button();
 	
+	private static final double TITLE_DISTANCE = Settings.getGuiHeigth()/10;
+	private static final double TITLE_HEIGTH = Settings.getGuiHeigth()/8;
+	private static final double TITLE_WIDTH = Settings.getGuiWidth();
+	private static final double TITLE_FONT_SIZE = TITLE_HEIGTH/1.5;
+	
+	
 	@Override
 	public final void init(final Controller controller, final View view) {
 		super.init(controller, view);
-		this.scoreLabel.setText(String.valueOf(this.getController().getScore()));
+		
+		this.gameOverLabel.setText("Game Over");
+		this.gameOverLabel.setFont(Font.font(TITLE_FONT_SIZE));
+		this.gameOverLabel.setAlignment(Pos.BOTTOM_CENTER);
+		this.gameOverLabel.setPrefSize(TITLE_WIDTH, TITLE_HEIGTH);
+		
+		
+		
+		/*this.scoreLabel.setText(String.valueOf(this.getController().getScore()));
 		this.destroyedBubbleLabel.setText(String.valueOf(this.getController().getDestroyedBubbles()));
-		this.gameTimeLabel.setText(String.valueOf(this.getController().getGameTime()));
+		this.gameTimeLabel.setText(String.valueOf(this.getController().getGameTime()));*/
 	}
 	
 	@Override
