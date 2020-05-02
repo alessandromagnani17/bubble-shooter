0090d6726c0c58e2efdc6e8e9dfda1b3ed6bc006
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-02 16:03:45 +0200

    Remove unused import

--- src/main/java/bubbleshooter/view/scene/controller/HighscoreController.java
+++ src/main/java/bubbleshooter/view/scene/controller/HighscoreController.java
@@ -1,18 +1,12 @@
 package bubbleshooter.view.scene.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import bubbleshooter.controller.Controller;
 import bubbleshooter.model.gamemodality.LevelTypes;
-import bubbleshooter.model.highscore.HighscoreStoreImpl;
 import bubbleshooter.model.highscore.HighscoreStructure;
 import bubbleshooter.view.View;
 import bubbleshooter.view.scene.FXMLPath;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
