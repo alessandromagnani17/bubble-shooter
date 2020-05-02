bd056964367c4fa5fcd3ce278d43351c4d3d2dcd
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-02 15:56:41 +0200

    Remove unused import

--- src/main/java/bubbleshooter/view/scene/controller/GameOverController.java
+++ src/main/java/bubbleshooter/view/scene/controller/GameOverController.java
@@ -9,8 +9,6 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.Background;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class GameOverController extends AbstractController {
