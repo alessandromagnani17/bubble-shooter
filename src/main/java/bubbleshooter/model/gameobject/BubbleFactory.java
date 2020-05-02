f78d055e2c43f243f83d9179f8adc4976e206017
giacomo accursi <giacomo.accursi@gmail.com>
2020-05-02 19:31:21 +0200

    now the methods aren't static

--- src/main/java/bubbleshooter/model/gameobject/BubbleFactory.java
+++ src/main/java/bubbleshooter/model/gameobject/BubbleFactory.java
@@ -4,16 +4,18 @@ import javafx.geometry.Point2D;
 
 public final class BubbleFactory {
 
-	private BubbleFactory() {
+	public BubbleFactory() {
 	}
-	
-	public static Bubble createGridBubble(final Point2D position) {
-		return new GridBubble(position); 
+
+	public Bubble createGridBubble(final Point2D position, BubbleColor color) {
+		return new GridBubble(position, color);
 	}
-	public static Bubble createShootingBubble(final Point2D position) {
-		return new ShootingBubble(position); 
+
+	public Bubble createShootingBubble(final Point2D position, BubbleColor color) {
+		return new ShootingBubble(position, color);
 	}
-	public static Bubble createSwitchBubble(final Point2D position) {
-		return new SwitchBubble(position); 
+
+	public Bubble createSwitchBubble(final Point2D position, BubbleColor color) {
+		return new SwitchBubble(position, color);
 	}
 }
