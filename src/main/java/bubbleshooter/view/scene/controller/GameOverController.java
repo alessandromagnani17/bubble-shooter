67cd1cd39587fa0c7765f1e3158d30709f227f6e
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-02 15:02:53 +0200

    Add all variable with their fx:ids

--- src/main/java/bubbleshooter/view/scene/controller/GameOverController.java
+++ src/main/java/bubbleshooter/view/scene/controller/GameOverController.java
@@ -4,16 +4,21 @@ import bubbleshooter.controller.Controller;
 import bubbleshooter.view.View;
 import bubbleshooter.view.scene.FXMLPath;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 public class GameOverController extends AbstractController {
 	
-	@FXML private TextArea textArea = new TextArea();
+	@FXML private Label gameOverLabel = new Label();
 	@FXML private Label scoreLabel = new Label();
+	@FXML private Label scoreLabelToWrite = new Label();
 	@FXML private Label destroyedBubbleLabel = new Label();
+	@FXML private Label destroyedBubbleLabelToWrite = new Label();
 	@FXML private Label gameTimeLabel = new Label();
-	@FXML private Label wrongShootsLabel = new Label();
+	@FXML private Label gameTimeLabelToWrite = new Label();
+	@FXML private TextArea textArea = new TextArea();
+	@FXML private Button buttonAdd = new Button();
 	
 	@Override
 	public final void init(final Controller controller, final View view) {
@@ -21,7 +26,6 @@ public class GameOverController extends AbstractController {
 		this.scoreLabel.setText(String.valueOf(this.getController().getScore()));
 		this.destroyedBubbleLabel.setText(String.valueOf(this.getController().getDestroyedBubbles()));
 		this.gameTimeLabel.setText(String.valueOf(this.getController().getGameTime()));
-		this.wrongShootsLabel.setText(String.valueOf(this.getController().getWrongShoots()));
 	}
 	
 	@Override
