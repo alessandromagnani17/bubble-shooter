6045651e25e68940414bb09be4ae170223039cea
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-02 21:38:52 +0200

    Add @FXML variables

--- src/main/java/bubbleshooter/view/scene/controller/PauseController.java
+++ src/main/java/bubbleshooter/view/scene/controller/PauseController.java
@@ -3,8 +3,16 @@ package bubbleshooter.view.scene.controller;
 import bubbleshooter.controller.Controller;
 import bubbleshooter.view.View;
 import bubbleshooter.view.scene.FXMLPath;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class PauseController extends AbstractController {
+	
+	@FXML private Label titleLabel = new Label();
+	@FXML private Button resumeButton = new Button();
+	@FXML private Button restartButton = new Button();
+	@FXML private Button quitButton = new Button();
 	
 	@Override
 	public final void init(final Controller controller, final View view) {
