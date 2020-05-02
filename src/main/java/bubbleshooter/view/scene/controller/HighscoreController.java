0927ef80f35242e95feeea8979dc4420a732c460
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-02 16:57:30 +0200

    Set layout property for all items

--- src/main/java/bubbleshooter/view/scene/controller/HighscoreController.java
+++ src/main/java/bubbleshooter/view/scene/controller/HighscoreController.java
@@ -21,7 +21,7 @@ public class HighscoreController extends AbstractController {
 	@FXML private Label titleLabel = new Label();
 	@FXML private Label basicModeLabel = new Label();
 	@FXML private Label survivalModeLabel = new Label();
-	@FXML private Button addButton = new Button();
+	@FXML private Button backMenuButton = new Button();
 	@FXML private TableView<HighscoreStructure> tableBaseView;
 	@FXML private TableColumn<HighscoreStructure, String> nameBaseColumn;
 	@FXML private TableColumn<HighscoreStructure, Integer> scoreBaseColumn;
@@ -29,12 +29,19 @@ public class HighscoreController extends AbstractController {
 	@FXML private TableColumn<HighscoreStructure, String> nameSurvivalColumn;
 	@FXML private TableColumn<HighscoreStructure, Integer> scoreSurvivalColumn;
 	
-	private static final double TITLE_DISTANCE = Settings.getGuiHeigth()/10;
+	private static final double TITLE_DISTANCE = Settings.getGuiHeigth()/40;
 	private static final double TITLE_HEIGTH = Settings.getGuiHeigth()/8;
 	private static final double TITLE_WIDTH = Settings.getGuiWidth();
 	private static final double TITLE_FONT_SIZE = TITLE_HEIGTH/1.5;
 	private static final double TABLE_HEIGTH = Settings.getGuiHeigth()/2;
 	private static final double TABLE_WIDTH = Settings.getGuiWidth()/2.2;
+	private static final double DETACHMENT = Settings.getGuiWidth() - 2*TABLE_WIDTH;
+	private static final double LABEL_HEIGTH = Settings.getGuiHeigth()/10;
+	private static final double LABEL_WIDTH = TABLE_WIDTH;
+	private static final double LABEL_FONT_SIZE = LABEL_HEIGTH/3;
+	private static final double BUTTON_WIDTH = TABLE_WIDTH - TABLE_WIDTH/4;
+	private static final double BUTTON_HEIGTH = Settings.getGuiHeigth()/10;
+	private static final double BUTTON_FONT_SIZE = BUTTON_HEIGTH/2.5;
 
 	@Override
 	public final void init(final Controller controller, final View view) {
@@ -45,7 +52,23 @@ public class HighscoreController extends AbstractController {
 		this.titleLabel.setAlignment(Pos.BOTTOM_CENTER);
 		this.titleLabel.setPrefSize(TITLE_WIDTH, TITLE_HEIGTH);
 		
+		this.basicModeLabel.setText("Basic Mode Highscores");
+		this.basicModeLabel.setFont(Font.font(LABEL_FONT_SIZE));
+		this.basicModeLabel.setLayoutX(DETACHMENT/4);
+		this.basicModeLabel.setLayoutY(TITLE_HEIGTH + TITLE_DISTANCE);
+		this.basicModeLabel.setPrefSize(LABEL_WIDTH, LABEL_HEIGTH);
+		
+		this.survivalModeLabel.setText("Survival Mode Highscores");
+		this.survivalModeLabel.setFont(Font.font(LABEL_FONT_SIZE));
+		this.survivalModeLabel.setLayoutX(TABLE_WIDTH + DETACHMENT/4 + DETACHMENT/2);
+		this.survivalModeLabel.setLayoutY(TITLE_HEIGTH + TITLE_DISTANCE);
+		this.survivalModeLabel.setPrefSize(LABEL_WIDTH, LABEL_HEIGTH);
+		
+		this.tableBaseView.setLayoutX(DETACHMENT/4);
+		this.tableBaseView.setLayoutY(this.basicModeLabel.getLayoutY() + LABEL_HEIGTH - DETACHMENT/4);
 		this.tableBaseView.setPrefSize(TABLE_WIDTH, TABLE_HEIGTH);
+		this.tableSurvivalView.setLayoutX(TABLE_WIDTH + DETACHMENT/4 + DETACHMENT/2);
+		this.tableSurvivalView.setLayoutY(this.survivalModeLabel.getLayoutY() + LABEL_HEIGTH - DETACHMENT/4);
 		this.tableSurvivalView.setPrefSize(TABLE_WIDTH, TABLE_HEIGTH);
 
 		this.nameBaseColumn.setCellValueFactory(new PropertyValueFactory<HighscoreStructure, String>("Name"));
@@ -57,11 +80,16 @@ public class HighscoreController extends AbstractController {
 		this.nameSurvivalColumn.setPrefWidth(TABLE_WIDTH/2);
 		this.scoreSurvivalColumn.setCellValueFactory(new PropertyValueFactory<HighscoreStructure, Integer>("Score"));
 		this.scoreSurvivalColumn.setPrefWidth(TABLE_WIDTH/2);
+		
+		this.backMenuButton.setText("Back to menu");
+		this.backMenuButton.setFont(Font.font(BUTTON_FONT_SIZE));
+		this.backMenuButton.setLayoutX(this.tableSurvivalView.getLayoutX() + TABLE_WIDTH/4);
+		this.backMenuButton.setLayoutY(this.tableSurvivalView.getLayoutY() + TABLE_HEIGTH + DETACHMENT/2);
+		this.backMenuButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGTH);
 
 		this.tableBaseView.setItems(getScores(LevelTypes.BASICMODE));
 		this.tableSurvivalView.setItems(getScores(LevelTypes.SURVIVALMODE));
 
-		// Rende le due tabelle non modificabili alla pressione del mouse
 		this.tableBaseView.setMouseTransparent(true);
 		this.tableSurvivalView.setMouseTransparent(true);
 
