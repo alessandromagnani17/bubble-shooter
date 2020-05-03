bfc50559b760a702f8e972089c5a8bf4334abae6
giacomo accursi <giacomo.accursi@gmail.com>
2020-05-03 18:05:10 +0200

    added remaining color to the switch method

--- src/main/java/bubbleshooter/model/gamemodality/AbstractGameMode.java
+++ src/main/java/bubbleshooter/model/gamemodality/AbstractGameMode.java
@@ -68,7 +68,8 @@ public abstract class AbstractGameMode {
 	}
 
 	public final void loadShootingBubble() {
-		
+		Random rnd = new Random();
+		List<BubbleColor> remainingColors = this.bubbleGridHelper.getRemainingColors();		
 		this.gameObjectManager.addBubble(Collections.singletonList(this.bubbleFactory.createShootingBubble(
 				new Point2D(Settings.getGuiWidth() / 2, Settings.getGuiHeigth() - Bubble.getWidth()),
 				remainingColors.get(rnd.nextInt(remainingColors.size() - 1)))));
@@ -76,7 +77,7 @@ public abstract class AbstractGameMode {
 
 	public final void loadSwitchBubble() {
 		Random rnd = new Random();
-		List<BubbleColor> remainingColors = this.bubbleGridHelper.getRemainingColors();
+		List<BubbleColor> remainingColors = this.bubbleGridHelper.getRemainingColors();		
 		this.gameObjectManager.addBubble(Collections.singletonList(this.bubbleFactory.createSwitchBubble(
 				new Point2D(Settings.getGuiWidth() / 1.1, Settings.getGuiHeigth() - Bubble.getWidth()),
 				remainingColors.get(rnd.nextInt(remainingColors.size() - 1)))));
