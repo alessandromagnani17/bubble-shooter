31ad2ae60b3599b6232337c321de80bb89b81716
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-03 17:28:29 +0200

    Add checkBounds()

--- src/main/java/bubbleshooter/controller/HandlerAdapterMouseMoved.java
+++ src/main/java/bubbleshooter/controller/HandlerAdapterMouseMoved.java
@@ -11,26 +11,46 @@ public class HandlerAdapterMouseMoved implements EventHandler<MouseEvent> {
 
     private Rotate cannonRotation = new Rotate();
     private Rotate lineRotation = new Rotate();
-    private DrawHelpLine helpLine;
+    private DrawHelpLine drawHelpLine;
     private double xBubble;
     private double yBubble;
 
     public HandlerAdapterMouseMoved(final Rotate cannonRotation, final Rotate lineRotation, final double xBubble, 
-    								final double yBubble, DrawHelpLine helpLine) {
+    								final double yBubble, DrawHelpLine drawHelpLine) {
     	this.cannonRotation = cannonRotation;
     	this.lineRotation = lineRotation;
         this.xBubble = xBubble;
         this.yBubble = yBubble;
-        this.helpLine = helpLine;
+        this.drawHelpLine = drawHelpLine;
     }
 
     @Override
     public final void handle(final MouseEvent event) {
         this.cannonRotation.setAngle(PhysicHelper.calculateAngle(event, this.xBubble, this.yBubble));
         this.lineRotation.setAngle(PhysicHelper.calculateAngle(event, this.xBubble, this.yBubble));
+        this.checkBounds(event.getX(), event.getY());
     }
 
-    public final double getRotationAngle() {
+    private void checkBounds(double xMouse, double yMouse) {
+    	double angularCoefficient, intercepts, xInt, yInt;
+    	
+    	angularCoefficient = PhysicHelper.calculateAngularCoefficient(this.drawHelpLine.getHelpLine().getStartX(), 
+    						 this.drawHelpLine.getHelpLine().getStartY(), xMouse, yMouse);
+    	intercepts = PhysicHelper.calculateIntercepts(this.drawHelpLine.getHelpLine().getStartX(), 
+				 			 this.drawHelpLine.getHelpLine().getStartY(), xMouse, yMouse);
+    	
+    	if(this.drawHelpLine.getBoundsLine().isVisible()) {
+    		this.drawHelpLine.getBoundsLine().setVisible(false);
+    	}
+    	
+		if(this.drawHelpLine.getHelpBounds().intersects(this.drawHelpLine.getLeftBounds())) {
+			xInt = 0;
+			yInt = intercepts;
+			
+		}
+	}
+
+	public final double getRotationAngle() {
         return this.cannonRotation.getAngle();
     }
 }
