d9f1c3c058c9f56164a79a06e0385ca4ccac83c5
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-03 16:22:31 +0200

    Add line rotation to constructor

--- src/main/java/bubbleshooter/controller/HandlerAdapterMouseMoved.java
+++ src/main/java/bubbleshooter/controller/HandlerAdapterMouseMoved.java
@@ -7,22 +7,25 @@ import javafx.scene.transform.Rotate;
 
 public class HandlerAdapterMouseMoved implements EventHandler<MouseEvent> {
 
-    private Rotate rotation = new Rotate();
+    private Rotate cannonRotation = new Rotate();
+    private Rotate lineRotation = new Rotate();
     private double xBubble;
     private double yBubble;
 
-    public HandlerAdapterMouseMoved(final Rotate rotation, final double xBubble, final double yBubble) {
-        this.rotation = rotation;
+    public HandlerAdapterMouseMoved(final Rotate cannonRotation, final Rotate lineRotation, final double xBubble, final double yBubble) {
+    	this.cannonRotation = cannonRotation;
+    	this.lineRotation = lineRotation;
         this.xBubble = xBubble;
         this.yBubble = yBubble;
     }
 
     @Override
     public final void handle(final MouseEvent event) {
-        this.rotation.setAngle(PhysicHelper.calculateAngle(event, this.xBubble, this.yBubble));
+        this.cannonRotation.setAngle(PhysicHelper.calculateAngle(event, this.xBubble, this.yBubble));
+        this.lineRotation.setAngle(PhysicHelper.calculateAngle(event, this.xBubble, this.yBubble));
     }
 
     public final double getRotationAngle() {
-        return this.rotation.getAngle();
+        return this.cannonRotation.getAngle();
     }
 }
