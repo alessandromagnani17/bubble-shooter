f3a249e0c6a2b9c2ee875c3ed8235e9590489103
Your Name <alessandro.magnani18@studio.unibo.it>
2020-05-04 22:06:57 +0200

    Ripositioned position of switch bubble

--- src/main/java/bubbleshooter/model/gameobject/SwitchBubble.java
+++ src/main/java/bubbleshooter/model/gameobject/SwitchBubble.java
@@ -1,7 +1,6 @@
 package bubbleshooter.model.gameobject;
 
 import bubbleshooter.model.component.ComponentType;
-import bubbleshooter.model.component.SwitchComponent;
 import javafx.geometry.Point2D;
 
 public class SwitchBubble extends AbstractBubble {
@@ -11,12 +10,11 @@ public class SwitchBubble extends AbstractBubble {
     }
 
     @Override
-    protected final void setComponents() {
-        this.addComponent(new SwitchComponent(this));
+    public final void update(final double elapsed) {
+        this.getComponent(ComponentType.SWITCHCOMPONENT).get().update(elapsed);
     }
 
     @Override
-    public final void update(final double elapsed) {
-        this.getComponent(ComponentType.SWITCHCOMPONENT).get().update(elapsed);
+    protected void setComponents() {
     }
 }
