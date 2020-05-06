11121847da8c2024c2a6b2ccf27f061a0bf232f3
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-06 22:04:53 +0200

    Add control for delete help line

--- src/main/java/bubbleshooter/view/scene/controller/GameController.java
+++ src/main/java/bubbleshooter/view/scene/controller/GameController.java
@@ -111,6 +111,8 @@ public class GameController extends AbstractController {
     public final void helpSelected() {
         if (this.helpCheckBox.isSelected()) {
             this.help.drawLine();
+        } else {
+        	this.help.deleteLine();
         }
     }
 
