1dfc3ed66032e3fccf52a492d2e44bff83661eb3
Andrea Acampora <andrea.acampora@studio.unibo.it>
2020-05-07 00:06:02 +0200

    Added methods to test the collisions

--- src/test/java/collision/TestBubbleCollision.java
+++ src/test/java/collision/TestBubbleCollision.java
@@ -1,15 +1,30 @@
 package collision;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import bubbleshooter.controller.Controller;
+import bubbleshooter.controller.ControllerImpl;
+import bubbleshooter.model.Model;
+import bubbleshooter.model.ModelImpl;
+import bubbleshooter.model.collision.Collision;
 import bubbleshooter.model.collision.CollisionController;
+import bubbleshooter.model.collision.CollisionHandler;
+import bubbleshooter.model.collision.GridCollisionHandler;
+import bubbleshooter.model.component.CollisionComponent;
+import bubbleshooter.model.component.ComponentType;
+import bubbleshooter.model.gamemodality.BasicMode;
+import bubbleshooter.model.gamemodality.GameMode;
+import bubbleshooter.model.gamemodality.LevelTypes;
 import bubbleshooter.model.gameobject.Bubble;
 import bubbleshooter.model.gameobject.BubbleColor;
 import bubbleshooter.model.gameobject.GridBubble;
 import bubbleshooter.model.gameobject.ShootingBubble;
+import bubbleshooter.view.View;
+import bubbleshooter.view.scene.FXMLPath;
 import javafx.geometry.Point2D;
 
 /**
@@ -17,19 +32,60 @@ import javafx.geometry.Point2D;
  */
 public class TestBubbleCollision {
 
-    private final Bubble gridBubble = new GridBubble(new Point2D(0, 0), BubbleColor.BLUE);
-    private final Bubble gridBubble2 = new GridBubble(new Point2D(99, 99), BubbleColor.RED);
-    private final Bubble shootingBubble = new ShootingBubble(new Point2D(100, 100) , BubbleColor.GREEN);
-    private static  CollisionController collisionController ;
-
-//    @BeforeAll
-//    public static final void init() {
-//        collisionController = new CollisionController();
-//    }
+    private final Model model = new ModelImpl();
+    private final View view = new TestView();
+    private final Controller controller = new ControllerImpl(model, view);
+    private final  CollisionController collisionController = this.model.getLevel().getCollisionController();
 
     @Test
-    public void testCollision() {
-       assertTrue(shootingBubble.getPosition().getX() > 0);
-    }
+    public final void testComponent() {
+        final Bubble bubble = new GridBubble(new Point2D(0, 0), BubbleColor.BLUE);
+        assertTrue(bubble.getComponent(ComponentType.COLLISIONCOMPONENT).isPresent());
+        final CollisionComponent component = (CollisionComponent) bubble.getComponent(ComponentType.COLLISIONCOMPONENT).get();
+        assertTrue(component.getContainer().equals(bubble));
+       }
 
+//    @Test
+//    public final void testCollisionController() {
+//    	this.controller.startGame(LevelTypes.BASICMODE);
+//        final Bubble shootingBubble = this.model.getGameObjectManager().getShootingBubble();
+//        final Bubble gridBubble = this.model.getGameObjectManager().getBubbleGrid().stream().findFirst().get();
+//        assertFalse(this.collisionController.hasCollided(gridBubble, shootingBubble));
+//        shootingBubble.setPosition(gridBubble.getPosition());
+//        assertTrue(this.collisionController.hasCollided(gridBubble, shootingBubble));
+//    }
+
+//    @Test
+//    public final void testCollisionHandler() {
+//        final Bubble gridBubble = new GridBubble(new Point2D(1, 1), BubbleColor.BLUE);
+//        final Bubble shootingBubble = new ShootingBubble(new Point2D(0, 0), BubbleColor.BLUE);
+//        final Collision collision = new Collision(gridBubble, shootingBubble);
+//        final CollisionHandler handler = new GridCollisionHandler(collision, new BasicMode());
+//    }
+
+    /**
+     * Anonymous class used only to Test the GameEngine and avoid all the initializations.
+     */
+    class TestView implements View {
+
+        @Override
+        public void launch(final Controller controller) {
+        }
+
+        @Override
+        public void loadScene(final FXMLPath scene) {
+        }
+
+        @Override
+        public void showGameOver() {
+        }
+
+        @Override
+        public void update() {
+        }
+
+        @Override
+        public void render() {
+        }
+    }
 }
