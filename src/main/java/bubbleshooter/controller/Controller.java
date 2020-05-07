74d70c16acc97b7455ea634d59f456fe19df9de5
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-07 12:35:00 +0200

    Remove unused import and methods

--- src/main/java/bubbleshooter/controller/Controller.java
+++ src/main/java/bubbleshooter/controller/Controller.java
@@ -4,9 +4,7 @@ import java.util.List;
 import bubbleshooter.controller.engine.GameLoop;
 import bubbleshooter.model.gamemodality.LevelTypes;
 import bubbleshooter.model.gameobject.Bubble;
-import bubbleshooter.model.highscore.HighscoreStructure;
 import bubbleshooter.model.highscore.ScoreManager;
-import javafx.collections.ObservableList;
 
 /**
  * Interface Controller used to dialogue with {@link Model} and {@link View} in order to respect MVC design pattern.
@@ -31,18 +29,6 @@ public interface Controller {
     GameLoop getGameEngine();
 
 	void saveScore(String text);
- 
-    /*int getScore();
-
-	ObservableList<HighscoreStructure> getHighscoreList(LevelTypes gameMode);
-
-	void saveScore(String text);
-
-	int getDestroyedBubbles();
-
-	double getGameTime();
-
-	int getWrongShoots();*/
 	
 	LevelTypes getCurrentLevel();
 
