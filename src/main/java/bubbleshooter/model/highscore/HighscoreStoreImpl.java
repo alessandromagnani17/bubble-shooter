331714a06e1d44d41b9c7568ff04ae141d680bdf
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-09 18:14:39 +0200

    Fix checkstyle warnings

--- src/main/java/bubbleshooter/model/highscore/HighscoreStoreImpl.java
+++ src/main/java/bubbleshooter/model/highscore/HighscoreStoreImpl.java
@@ -33,84 +33,84 @@ public class HighscoreStoreImpl implements HighscoreStore {
     private static final int CAPACITY = 10;
     private boolean flag;
 
-	/**
-	 * Constructor of the {@link HighscoreStore} used for create the file if necessary and
-	 * for allocate the HashMap.
-	 */
-	public HighscoreStoreImpl() {
-		this.file = new File(DIR_PATH + FILE_PATH);
-		try {
-			if (!file.getParentFile().exists()) {
-				file.getParentFile().mkdirs();
-			}
-			if (!file.exists()) {
-				this.file.createNewFile();
-				flag = true;
-			} else {
-				flag = false;
-			}
-			if (flag) {
-				FileWriter fw = new FileWriter(this.file);
-				BufferedWriter bw = new BufferedWriter(fw);
+    /**
+     * Constructor of the {@link HighscoreStore} used for create the file if necessary and
+     * for allocate the HashMap.
+     */
+    public HighscoreStoreImpl() {
+        this.file = new File(DIR_PATH + FILE_PATH);
+        try {
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+		    }
+            if (!file.exists()) {
+                this.file.createNewFile();
+                flag = true;
+            } else {
+                flag = false;
+            }
+            if (flag) {
+                FileWriter fw = new FileWriter(this.file);
+                BufferedWriter bw = new BufferedWriter(fw);
 
-				bw.write("HIGHSCORES!!\n\n");
-				bw.write("END_OF_HIGH_END_OF_HIGH\n\n");
-				bw.write("SURVIVAL_MODE_HIGHSCORES...\n");
-				bw.write("END_OF_HIGH_END_OF_HIGH\n\n");
-				bw.write("END_OF_FILE_END_OF_FILE");
+                bw.write("HIGHSCORES!!\n\n");
+                bw.write("END_OF_HIGH_END_OF_HIGH\n\n");
+                bw.write("SURVIVAL_MODE_HIGHSCORES...\n");
+                bw.write("END_OF_HIGH_END_OF_HIGH\n\n");
+                bw.write("END_OF_FILE_END_OF_FILE");
 
-				bw.flush();
-				bw.close();
-			}
+                bw.flush();
+                bw.close();
+            }
 
-		} catch (IOException e) {
-			System.out.println("ERROR !!! Can't create file...");
-			e.printStackTrace();
-		}
-		this.mapOfItems = new HashMap<>();
-	}
+        } catch (IOException e) {
+            System.out.println("ERROR !!! Can't create file...");
+            e.printStackTrace();
+        }
+        this.mapOfItems = new HashMap<>();
+    }
 
-	/**
+    /**
      * Method for get the file where scores are saved.
      * 
      * @return the file where the scores are saved.
      */	
-	@Override
-	public final File getFile() {
-		return this.file;
-	}
+    @Override
+    public final File getFile() {
+        return this.file;
+    }
 
-	/**
+    /**
      * Method for add a score for a game modality.
      * 
      * @param score the current {@link HighscoreStructure} to save.
      */
-	@Override
-	public final void addScore(final HighscoreStructure score) {
+    @Override
+    public final void addScore(final HighscoreStructure score) {
 
-		this.mapOfItems = readFile();
+        this.mapOfItems = readFile();
 
-		this.mapOfItems.get(score.getGameMode()).add(score);
+        this.mapOfItems.get(score.getGameMode()).add(score);
 
-		sort(this.mapOfItems.get(score.getGameMode()));
+        sort(this.mapOfItems.get(score.getGameMode()));
 
-		clean(this.mapOfItems.get(score.getGameMode()));
+        clean(this.mapOfItems.get(score.getGameMode()));
 
-		reWriteFile();
-	}
+        reWriteFile();
+    }
 
     /**
      * Private method used for put in map every the type of scores saved.
      * 
      * @return the map contains the different scores.
      */
-	private Map<GameType, List<HighscoreStructure>> readFile() {
-		Map<GameType, List<HighscoreStructure>> map = new HashMap<>();
-		for (GameType tipo : GameType.values()) {
-			map.put(tipo, readFromFile(tipo));
-		}
-		return map;
-	}
+    private Map<GameType, List<HighscoreStructure>> readFile() {
+        Map<GameType, List<HighscoreStructure>> map = new HashMap<>();
+        for (GameType tipo : GameType.values()) {
+            map.put(tipo, readFromFile(tipo));
+        }
+        return map;
+    }
 
     /**
      * Private method used for read from file a list of scores for a specific game modality.
@@ -119,38 +119,38 @@ public class HighscoreStoreImpl implements HighscoreStore {
      * 
      * @return the list of scores for the specific game modality.
      */
-	private List<HighscoreStructure> readFromFile(final GameType gameMode) {
+    private List<HighscoreStructure> readFromFile(final GameType gameMode) {
 
-		List<HighscoreStructure> itemsSet = new ArrayList<>();
-		String modality = whichMod(gameMode);
-		String readString;
+        List<HighscoreStructure> itemsSet = new ArrayList<>();
+        String modality = whichMod(gameMode);
+        String readString;
 
-		try {
-			FileReader fr = new FileReader(file);
-			BufferedReader br = new BufferedReader(fr);
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
 
-			readString = br.readLine();
-			while (!readString.equals("END_OF_FILE_END_OF_FILE")) {
-				readString = br.readLine();
-				if (readString.equals(modality)) {
-					readString = br.readLine();
-					while (!readString.equals("END_OF_HIGH_END_OF_HIGH")) {
-						itemsSet.add(generateHighscore(readString, gameMode));
-						readString = br.readLine();
-					}
-				}
+            readString = br.readLine();
+            while (!readString.equals("END_OF_FILE_END_OF_FILE")) {
+                readString = br.readLine();
+                if (readString.equals(modality)) {
+                    readString = br.readLine();
+                    while (!readString.equals("END_OF_HIGH_END_OF_HIGH")) {
+                        itemsSet.add(generateHighscore(readString, gameMode));
+                        readString = br.readLine();
+                    }
+                }
 
-			}
+            }
 
-			br.close();
+            br.close();
 
-		} catch (IOException e) {
-			System.out.println("ERROR !!! Can't create file...");
-			e.printStackTrace();
-		}
+        } catch (IOException e) {
+            System.out.println("ERROR !!! Can't create file...");
+            e.printStackTrace();
+        }
 
-		return itemsSet;
-	}
+        return itemsSet;
+    }
 
     /**
      * Private method used for convert the @param gameMode to a String.
@@ -159,16 +159,16 @@ public class HighscoreStoreImpl implements HighscoreStore {
      * 
      * @return the String for the game modality.
      */
-	private String whichMod(final GameType gameMode) {
-		switch (gameMode) {
-		case BASICMODE:
-			return "BASIC_MODE_HIGHSCORES...";
-		case SURVIVALMODE:
-			return "SURVIVAL_MODE_HIGHSCORES...";
-		default:
-			return null;
-		}
-	}
+    private String whichMod(final GameType gameMode) {
+        switch (gameMode) {
+        case BASICMODE:
+            return "BASIC_MODE_HIGHSCORES...";
+	    case SURVIVALMODE:
+	        return "SURVIVAL_MODE_HIGHSCORES...";
+	    default:
+	        return null;
+	    }
+    }
 
     /**
      * Private method used for generate a {@link HighscoreStructure} for add into a list.
@@ -178,110 +178,109 @@ public class HighscoreStoreImpl implements HighscoreStore {
      * 
      * @return the {@link HighscoreStructure}.
      */
-	private HighscoreStructure generateHighscore(final String readString, final GameType gameMode) {
-		String name = "", score = "";
-		char space = ' ';
-		boolean flag = true;
+    private HighscoreStructure generateHighscore(final String readString, final GameType gameMode) {
+        String name = "", score = "";
+        char space = ' ';
+        boolean flag = true;
 
-		for (int i = 0; i < readString.length(); i++) {
+        for (int i = 0; i < readString.length(); i++) {
 
-			if (!flag) {
-				score = score + readString.charAt(i);
-			}
+            if (!flag) {
+                score = score + readString.charAt(i);
+            }
 
-			if (!(readString.charAt(i) == space) && flag) {
-				name = name + readString.charAt(i);
-			} else {
-				flag = false;
-			}
-		}
+            if (!(readString.charAt(i) == space) && flag) {
+                name = name + readString.charAt(i);
+            } else {
+                flag = false;
+            }
+        }
 
-		return new HighscoreStructure(name, Integer.parseInt(score), gameMode);
-	}
+        return new HighscoreStructure(name, Integer.parseInt(score), gameMode);
+    }
 
     /**
      * Private method used for sort a list of {@link HighscoreStructure}.
      * 
      * @param itemsSet a list of {@link HighscoreStructure}.
      */
-	private void sort(final List<HighscoreStructure> itemsSet) {
-		Comparator<HighscoreStructure> comp = new Comparator<>() {
-			@Override
-			public int compare(final HighscoreStructure o1, final HighscoreStructure o2) {
-				return o2.getScore() - o1.getScore();
-			}
-		};
-		Collections.sort(itemsSet, comp);
-	}
+    private void sort(final List<HighscoreStructure> itemsSet) {
+        Comparator<HighscoreStructure> comp = new Comparator<>() {
+            @Override
+            public int compare(final HighscoreStructure o1, final HighscoreStructure o2) {
+                return o2.getScore() - o1.getScore();
+            }
+        };
+        Collections.sort(itemsSet, comp);
+    }
 
     /**
      * Private method used for clean a list if necessary.
      * 
      * @param itemsSet the list to clean.
      */
-	private void clean(final List<HighscoreStructure> itemsSet) {
-		for (int i = 0; i < itemsSet.size(); i++) {
-			if (i >= CAPACITY) {
-				itemsSet.remove(i);
-			}
-		}
-	}
+    private void clean(final List<HighscoreStructure> itemsSet) {
+        for (int i = 0; i < itemsSet.size(); i++) {
+            if (i >= CAPACITY) {
+                itemsSet.remove(i);
+            }
+        }
+    }
 
     /**
      * Private method used for re-writing the file with the new modify.
      */
-	private void reWriteFile() {
-		String stringToWrite;
-		try {
-			this.file.delete();
-			if (!file.getParentFile().exists()) {
-				file.getParentFile().mkdirs();
-			}
-			if (!file.exists()) {
-				this.file.createNewFile();
-			}
+    private void reWriteFile() {
+        String stringToWrite;
+        try {
+            this.file.delete();
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            if (!file.exists()) {
+                this.file.createNewFile();
+            }
 
-			FileWriter fw = new FileWriter(this.file);
-			BufferedWriter bw = new BufferedWriter(fw);
+            FileWriter fw = new FileWriter(this.file);
+            BufferedWriter bw = new BufferedWriter(fw);
 
-			bw.write("HIGHSCORES!!\n\n");
-			bw.write("BASIC_MODE_HIGHSCORES...\n");
-			for (HighscoreStructure o : this.mapOfItems.get(GameType.BASICMODE)) {
-				stringToWrite = o.getName() + " " + String.valueOf(o.getScore()) + "\n";
-				bw.write(stringToWrite);
-			}
-			bw.write("END_OF_HIGH_END_OF_HIGH\n\n");
-			bw.write("SURVIVAL_MODE_HIGHSCORES...\n");
-			for (HighscoreStructure o : this.mapOfItems.get(GameType.SURVIVALMODE)) {
-				stringToWrite = o.getName() + " " + String.valueOf(o.getScore()) + "\n";
-				bw.write(stringToWrite);
-			}
-			bw.write("END_OF_HIGH_END_OF_HIGH\n\n");
-			bw.write("END_OF_FILE_END_OF_FILE");
+            bw.write("HIGHSCORES!!\n\n");
+            bw.write("BASIC_MODE_HIGHSCORES...\n");
+            for (HighscoreStructure o : this.mapOfItems.get(GameType.BASICMODE)) {
+                stringToWrite = o.getName() + " " + String.valueOf(o.getScore()) + "\n";
+                bw.write(stringToWrite);
+            }
+            bw.write("END_OF_HIGH_END_OF_HIGH\n\n");
+            bw.write("SURVIVAL_MODE_HIGHSCORES...\n");
+            for (HighscoreStructure o : this.mapOfItems.get(GameType.SURVIVALMODE)) {
+                stringToWrite = o.getName() + " " + String.valueOf(o.getScore()) + "\n";
+                bw.write(stringToWrite);
+            }
+            bw.write("END_OF_HIGH_END_OF_HIGH\n\n");
+            bw.write("END_OF_FILE_END_OF_FILE");
 
-			bw.flush();
-			bw.close();
+            bw.flush();
+            bw.close();
 
-		} catch (IOException e) {
-			System.out.println("Error in re-writing of the file !!!");
-			e.printStackTrace();
-		}
+        } catch (IOException e) {
+            System.out.println("Error in re-writing of the file !!!");
+            e.printStackTrace();
+        }
+    }
 
-	}
-
-	/**
+    /**
      * Method to have a list of scores for a specific game modality.
      * 
      * @param gameMode game modality which we want the scores.
      * @return the scores for a game modality.
      */
-	@Override
-	public final ObservableList<HighscoreStructure> getHighscoresForModality(final GameType gameMode) {
-		ObservableList<HighscoreStructure> result = FXCollections.observableArrayList();
-		this.mapOfItems = readFile();
-		if (this.mapOfItems.containsKey(gameMode)) {
-			result.addAll(this.mapOfItems.get(gameMode));
-		}
-		return result;
-	}
+    @Override
+    public final ObservableList<HighscoreStructure> getHighscoresForModality(final GameType gameMode) {
+        ObservableList<HighscoreStructure> result = FXCollections.observableArrayList();
+        this.mapOfItems = readFile();
+        if (this.mapOfItems.containsKey(gameMode)) {
+            result.addAll(this.mapOfItems.get(gameMode));
+        }
+        return result;
+    }
 }
