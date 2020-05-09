7b26748ed2bd5a4f57e53d64824e4c373aee819b
Your Name <alessandro.magnani18@studio.unibo.it>
2020-05-09 19:46:15 +0200

    Added javadoc

--- src/main/java/bubbleshooter/model/game/GameSwitcher.java
+++ src/main/java/bubbleshooter/model/game/GameSwitcher.java
@@ -1,19 +1,30 @@
 package bubbleshooter.model.game;
 
 import java.util.List;
-
 import bubbleshooter.model.bubble.Bubble;
 import bubbleshooter.model.bubble.BubbleColor;
 import bubbleshooter.model.bubble.BubbleType;
 
+/**
+ * Class that switches the {@link SwitchBubble} with the {@link ShootingBubble}.
+ * Used by {@link SwitcherController}.
+ */
 public class GameSwitcher {
 
     private final List<Bubble> bubbles;
 
+    /**
+     * Constructor for a new GameSwitcher.
+     * 
+     * @param bubbles , the list of all {@link Bubble}s.
+     */
     public GameSwitcher(final List<Bubble> bubbles) {
         this.bubbles = bubbles;
     }
 
+    /**
+     * Method that switches the {@link SwitchBubble} with the {@link ShootingBubble}.
+     */
     public final void switchBall() {
         final BubbleColor bubbleColor = bubbles.stream()
                                                .filter(a -> a.getType().equals(BubbleType.SHOOTING_BUBBLE))
