095d8cc1744e2fefa8b87130cc0f79b1cb393bb7
Andrea Acampora <andrea.acampora@studio.unibo.it>
2020-05-10 13:44:06 +0200

    Fixed some checkstyle warnings

--- src/main/java/bubbleshooter/utility/PhysicHelper.java
+++ src/main/java/bubbleshooter/utility/PhysicHelper.java
@@ -5,9 +5,9 @@ import javafx.geometry.Point2D;
 
  public final class PhysicHelper {
 
-	private static final double MAXANGLE =  75.0;
-	private static final double MINANGLE = -75.0;
-	
+    private static final double MAXANGLE =  75.0;
+    private static final double MINANGLE = -75.0;
+
 
     private PhysicHelper() {
     }
@@ -25,7 +25,7 @@ import javafx.geometry.Point2D;
 
     public static double calculateAngle(final Point2D eventPosition, final Point2D shootingBubblePosition) {
         final double hypotenuse = Math.sqrt(Math.pow(eventPosition.getX() - shootingBubblePosition.getX(), 2) 
-        		          + Math.pow(eventPosition.getY() - shootingBubblePosition.getY(), 2));
+                                + Math.pow(eventPosition.getY() - shootingBubblePosition.getY(), 2));
         final double cathetus = eventPosition.getX() - shootingBubblePosition.getX();
         return checkAngle(Math.toDegrees(Math.asin(cathetus / hypotenuse)));
     }
@@ -39,23 +39,23 @@ import javafx.geometry.Point2D;
         return angle;
     }
 
-	public static double calculateAngularCoefficient(final Point2D startPointSecondLine, final Point2D endPointSecondLine) {
-		return (endPointSecondLine.getY() - startPointSecondLine.getY()) / (endPointSecondLine.getX() - startPointSecondLine.getX());
-	}
+    public static double calculateAngularCoefficient(final Point2D startPointSecondLine, final Point2D endPointSecondLine) {
+        return (endPointSecondLine.getY() - startPointSecondLine.getY()) / (endPointSecondLine.getX() - startPointSecondLine.getX());
+    }
 
-	public static double calculateIntercepts(final Point2D startPointSecondLine, final Point2D endPointSecondLine) {
-		return (endPointSecondLine.getX() * startPointSecondLine.getY() 
-			  - startPointSecondLine.getX() * endPointSecondLine.getY()) / (endPointSecondLine.getX() - startPointSecondLine.getX());
-	}
+    public static double calculateIntercepts(final Point2D startPointSecondLine, final Point2D endPointSecondLine) {
+        return (endPointSecondLine.getX() * startPointSecondLine.getY() 
+              - startPointSecondLine.getX() * endPointSecondLine.getY()) / (endPointSecondLine.getX() - startPointSecondLine.getX());
+    }
 
-	public static boolean angleTooHigh(final Point2D startPointFirstLine, final Point2D startPointSecondLine) {
-		double angle;
-		angle = PhysicHelper.calculateAngle(startPointFirstLine, startPointSecondLine);
-		if (angle > MAXANGLE - 0.01) {
+    public static boolean angleTooHigh(final Point2D startPointFirstLine, final Point2D startPointSecondLine) {
+        double angle;
+        angle = PhysicHelper.calculateAngle(startPointFirstLine, startPointSecondLine);
+        if (angle > MAXANGLE - 0.01) {
             return false;
         } else if (angle < MINANGLE + 0.01) {
             return false;
         }
-		return true;
-	}
+        return true;
+    }
 }
