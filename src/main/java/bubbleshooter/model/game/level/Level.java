bc3b9ea9f1a4ac6514d40acd46fa6e42817e5949
giacomo accursi <giacomo.accursi@gmail.com>
2020-05-11 21:58:21 +0200

    refactoring and add comments and javadoc

--- src/main/java/bubbleshooter/model/game/level/Level.java
+++ src/main/java/bubbleshooter/model/game/level/Level.java
@@ -13,121 +13,118 @@ import bubbleshooter.model.game.GameStatus;
 
 /**
  * Represents the game. It's responsible to initialize all {@link Bubble}s and
- * create new row of bubbles
+ * create new row of bubbles.
  */
 
 public interface Level {
 
 	/**
-	 * starts selected game and initialize all bubbles
+	 * Starts selected game and initialize all bubbles.
 	 */
 	void start();
 
 	/**
-	 * updates the game
+	 * Updates the game.
 	 * 
-	 * @param elapsed
+	 * @param elapsed.
 	 */
 	void update(double elapsed);
 
 	/**
-	 * gets the {@link BubblesManager}
+	 * Gets the {@link BubblesManager}.
 	 * 
-	 * @return {@link BubblesManager}
+	 * @return {@link BubblesManager}.
 	 */
 	BubblesManager getBubblesManager();
 
 	/**
-	 * loads the {@link ShootingBubble}
+	 * Loads the {@link ShootingBubble}.
 	 */
 	void loadShootingBubble();
 
 	/**
-	 * loads the {@link SwitchBubble}
+	 * Loads the {@link SwitchBubble}.
 	 */
 	void loadSwitchBubble();
 
 	/**
-	 * sets current {@link LevelType}
+	 * Sets current {@link LevelType}.
 	 * 
-	 * @param the {@link LevelType}
+	 * @param the {@link LevelType}.
 	 */
 	void setCurrentGameType(LevelType gameType);
 
 	/**
-	 * sets the {@link GameStatus}
+	 * Sets the {@link GameStatus}.
 	 * 
-	 * @param the {@link GameStatus}
+	 * @param the {@link GameStatus}.
 	 */
 	void setGameStatus(GameStatus status);
 
 	/**
-	 * gets the {@link GameStatus}
+	 * Gets the {@link GameStatus}.
 	 * 
-	 * @return the {@link GameStatus}
+	 * @return the {@link GameStatus}.
 	 */
 	GameStatus getGameStatus();
 
 	/**
-	 * gets the {@link BubblesManager}
+	 * Gets the {@link BubblesManager}.
 	 * 
-	 * @return {@link BubblesManager}
+	 * @return {@link BubblesManager}.
 	 */
 	BubbleGridManager getGridManager();
 
 	/**
-	 * gets the {@link BubbleGridHelper}
+	 * Gets the {@link BubbleGridHelper}.
 	 * 
-	 * @return the {@link BubbleGridHelper}
+	 * @return the {@link BubbleGridHelper}.
 	 */
 	BubbleGridHelper getGridHelper();
 
 	/**
-	 * gets the {@link CollisionController}
+	 * Gets the {@link CollisionController}.
 	 * 
-	 * @return the {@link CollisionController}
+	 * @return the {@link CollisionController}.
 	 */
 	CollisionController getCollisionController();
 
 	/**
-	 * returns a list of current {@link Bubble}s
+	 * Returns a list of current {@link Bubble}s.
 	 * 
-	 * @return a {@link List} of all {@link Bubble}s
+	 * @return a {@link List} of all {@link Bubble}s.
 	 */
 	List<Bubble> getCurrentBubbles();
 
 	/**
-	 * gets the {@link GameInfoManager}
+	 * Gets the {@link GameInfoManager}.
 	 * 
-	 * @return the {@link GameInfoManager}
+	 * @return the {@link GameInfoManager}.
 	 */
 	GameInfoManager getGameInfoManager();
 
 	/**
-	 * gets the current {@link LevelType}
+	 * Gets the current {@link LevelType}.
 	 * 
-	 * @return the {@link LevelType}
+	 * @return the {@link LevelType}.
 	 */
 	LevelType getCurrentGameType();
 
 	/**
-	 * gets the {@link BubbleFactory}
+	 * Gets the {@link BubbleFactory}.
 	 * 
-	 * @return the {@link BubbleFactory}
+	 * @return the {@link BubbleFactory}.
 	 */
 	BubbleFactory getBubbleFactory();
-
+	
 	/**
-	 * gets the number of rows of {@link Bubble}s at the beginning of the game
 	 * 
-	 * @return the number of rows
-	 */
-	int getNumRows();
-
-	/**
-	 * gets the number of {@link Bubble}s per row at the beginning of the game
-	 * 
-	 * @return the number of {@link Bubble}s per row
+	 * @return
 	 */
 	int getBubblesPerRow();
+	
+	int getNumRow(); 
+	
+	
+
 }
