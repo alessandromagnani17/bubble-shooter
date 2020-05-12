a5a42f328f11ce7d637dba286889f039bcbfb240
Simone Montanari <simone.montanari14@studio.unibo.it>
2020-05-12 17:46:54 +0200

    Fix error in height

--- src/main/java/bubbleshooter/view/scene/controller/PauseController.java
+++ src/main/java/bubbleshooter/view/scene/controller/PauseController.java
@@ -23,14 +23,14 @@ public class PauseController extends AbstractController {
     @FXML private Button quitButton = new Button();
 
     private static final double TITLE_DISTANCE = Settings.getGuiHeight() / 10;
-    private static final double TITLE_HEIGTH = Settings.getGuiHeight() / 8;
+    private static final double TITLE_HEIGHT = Settings.getGuiHeight() / 8;
     private static final double TITLE_WIDTH = Settings.getGuiWidth();
-    private static final double TITLE_FONT_SIZE = TITLE_HEIGTH / 1.5;
+    private static final double TITLE_FONT_SIZE = TITLE_HEIGHT / 1.5;
     private static final double BUTTON_DISTANCE = Settings.getGuiHeight() / 50;
     private static final double BUTTON_WIDTH = Settings.getGuiWidth() / 2;
-    private static final double BUTTON_HEIGTH = Settings.getGuiHeight() / 10;
+    private static final double BUTTON_HEIGHT = Settings.getGuiHeight() / 10;
     private static final double BUTTON_X = Settings.getGuiWidth() / 2 - BUTTON_WIDTH / 2;
-    private static final double BUTTON_FONT_SIZE = BUTTON_HEIGTH / 2.5;
+    private static final double BUTTON_FONT_SIZE = BUTTON_HEIGHT / 2.5;
 
     @Override
     public final void init(final Controller controller, final View view) {
@@ -39,27 +39,27 @@ public class PauseController extends AbstractController {
         this.titleLabel.setText("Pause");
         this.titleLabel.setFont(Font.font(TITLE_FONT_SIZE));
         this.titleLabel.setAlignment(Pos.BOTTOM_CENTER);
-        this.titleLabel.setPrefSize(TITLE_WIDTH, TITLE_HEIGTH);
+        this.titleLabel.setPrefSize(TITLE_WIDTH, TITLE_HEIGHT);
 
         this.resumeButton.setText("Resume");
         this.resumeButton.setTextAlignment(TextAlignment.CENTER);
         this.resumeButton.setLayoutX(BUTTON_X);
-        this.resumeButton.setLayoutY(TITLE_HEIGTH + TITLE_DISTANCE);
-        this.resumeButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGTH);
+        this.resumeButton.setLayoutY(TITLE_HEIGHT + TITLE_DISTANCE);
+        this.resumeButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
         this.resumeButton.setFont(Font.font(BUTTON_FONT_SIZE));
 
         this.restartButton.setText("Restart");
         this.restartButton.setTextAlignment(TextAlignment.CENTER);
         this.restartButton.setLayoutX(BUTTON_X);
-        this.restartButton.setLayoutY(this.resumeButton.getLayoutY() + BUTTON_HEIGTH + BUTTON_DISTANCE);
-        this.restartButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGTH);
+        this.restartButton.setLayoutY(this.resumeButton.getLayoutY() + BUTTON_HEIGHT + BUTTON_DISTANCE);
+        this.restartButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
         this.restartButton.setFont(Font.font(BUTTON_FONT_SIZE));
 
         this.quitButton.setText("Quit");
         this.quitButton.setTextAlignment(TextAlignment.CENTER);
         this.quitButton.setLayoutX(BUTTON_X);
-        this.quitButton.setLayoutY(this.restartButton.getLayoutY() + BUTTON_HEIGTH + BUTTON_DISTANCE);
-        this.quitButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGTH);
+        this.quitButton.setLayoutY(this.restartButton.getLayoutY() + BUTTON_HEIGHT + BUTTON_DISTANCE);
+        this.quitButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
         this.quitButton.setFont(Font.font(BUTTON_FONT_SIZE));
 
     }
