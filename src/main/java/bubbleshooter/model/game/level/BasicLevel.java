d30e2bc9132bb8f9b5ac9f4dcd12234de3fcb1aa
giacomo accursi <giacomo.accursi@gmail.com>
2020-05-13 22:50:58 +0200

    refactoring

--- src/main/java/bubbleshooter/model/game/level/BasicLevel.java
+++ src/main/java/bubbleshooter/model/game/level/BasicLevel.java
@@ -5,7 +5,7 @@ import bubbleshooter.model.game.GameData;
 public class BasicLevel extends AbstractLevel {
 
     private static final int BUBBLE_SCORE = 20;
-    private static final int WRONG_SHOTS_BEFORE_NEW_ROW = 5;
+    private static final int WRONG_SHOOTS_BEFORE_NEW_ROW = 5;
 
     public BasicLevel() {
         this.setLevelType(LevelType.BASICMODE);
@@ -13,15 +13,15 @@ public class BasicLevel extends AbstractLevel {
 
     @Override
     public final void updateScore(final double elapsed) {
-        final GameData infoManager = this.getGameData();
-        infoManager.updateScore(infoManager.getDestroyedBubbles() * BUBBLE_SCORE);
+        final GameData gameData = this.getGameData();
+        gameData.updateScore(gameData.getDestroyedBubbles() * BUBBLE_SCORE);
     }
 
     @Override
     public final boolean isTimeToNewRow(final double elapsed) {
-        final GameData infoManager = this.getGameData();
-        if (infoManager.getWrongShoots() == WRONG_SHOTS_BEFORE_NEW_ROW) {
-            infoManager.clearWrongShoots();
+        final GameData gameData = this.getGameData();
+        if (gameData.getWrongShoots() == WRONG_SHOOTS_BEFORE_NEW_ROW) {
+            gameData.clearWrongShoots();
             return true;
         }
         return false;
